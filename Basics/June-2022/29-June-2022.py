4249f58dc478185f3d70a2ade17e32e134e3c506
rakesh050791 <rakesh050791@gmail.com>
2022-06-30 00:01:46 +0200

    perfect number program in python

--- Basics/June-2022/29-June-2022.py
+++ Basics/June-2022/29-June-2022.py
@@ -13,3 +13,35 @@ def main(num):
 if __name__ == '__main__':
     num = int(input())
     main(num)
+
+
+
+
+# 2 : You are given N positive integers.
+
+# For each given integer A, you have to tell whether it is a perfect number or not.
+
+# Perfect number is a positive integer which is equal to the sum of its proper positive divisors.
+def main(n):
+    
+    while n >= 1:
+        sum = 0
+
+        num = int(input())
+
+        for i in range(1, num-1):
+            if num % i == 0:
+                sum += i
+        if sum == num:
+            print('YES')
+        else:
+            print('NO')
+        n -= 1
+
+    
+if __name__ == '__main__':
+    n = int(input())
+    main(n)
+
+
+# 3 : 
