2773b64c8c574805ff02e510bf2554eb669f1af2
rakesh050791 <rakesh050791@gmail.com>
2022-07-10 01:59:06 +0200

    Good Pair problem solution in python

--- Basics/July-2022/06-July-2022.py
+++ Basics/July-2022/06-July-2022.py
@@ -30,3 +30,20 @@ if __name__ == '__main__':
     main()
 
 
+
+# 2 : Given an array A and an integer B. 
+# A pair(i, j) in the array is a good pair if i != j and (A[i] + A[j] == B). 
+# Check if any good pair exist or not.
+
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        for i in range(0, len(A)-1):
+            for j in range(i+1, len(A)):
+                if (A[i] + A[j] == B):
+                    return 1
+        return 0
+
+
