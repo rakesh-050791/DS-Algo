ede085420db41c0867accc272d85d2f2386d36f3
rakesh050791 <rakesh050791@gmail.com>
2022-07-11 02:22:03 +0200

    Calculating Range Sum Query

--- Basics/July-2022/08-July-2022.py
+++ Basics/July-2022/08-July-2022.py
@@ -39,4 +39,41 @@ class Solution:
         
         return minIndex if count > 0 else -1
 
+# 2 : You are given an integer array A of length N.
+# You are also given a 2D integer array B with dimensions M x 2, where each row denotes a [L, R] query.
+# For each query, you have to find the sum of all elements from L to R indices in A (1 - indexed).
+# More formally, find A[L] + A[L + 1] + A[L + 2] +... + A[R - 1] + A[R] for each query.
+
+# Input 1:
+# A = [1, 2, 3, 4, 5]
+# B = [[1, 4], [2, 3]]
+
+class Solution:
+    # @param A : list of integers
+    # @param B : list of list of integers
+     # @return an list of long
+    def rangeSum(self, A, B):
+        bLen = len(B)
+        aLen = len(A)
+
+        prefixSum = []
+        prefixSum.append(A[0])
+
+        for i in range(1, aLen):
+            prefixSum.append(prefixSum[i - 1] + A[i])
+
+        finalOutput = [0]*bLen
+
+        for i in range(bLen):
+            start = B[i][0]
+            end = B[i][1]
+
+            if start == 1:
+                finalOutput[i] = prefixSum[end - 1]
+            else:
+                result = prefixSum[end - 1] - prefixSum[start-1-1]
+                finalOutput[i]  = result
+                
+        return(finalOutput)
+
             
