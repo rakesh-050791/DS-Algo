815c6e804afa43d5931adb48022a0d58ed42d317
rakesh050791 <rakesh050791@gmail.com>
2022-07-15 17:34:47 +0200

    Amazing Subarrays

--- Basics/July-2022/13-July-2022.py
+++ Basics/July-2022/13-July-2022.py
@@ -119,6 +119,27 @@ class Solution:
         return "NO"
 
 
+# 6 : Amazing Subarrays
+
+# You are given a string S, and you have to find all the amazing substrings of S.
+
+# An amazing Substring is one that starts with a vowel (a, e, i, o, u, A, E, I, O, U).
+
+class Solution:
+    # @param A : string
+    # @return an integer
+    def solve(self, A):
+        Vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+        N = len(A)
+
+        ans = 0
+        for i in range(N):
+            if A[i] in Vowels:
+                length = N - i
+                ans += length
+        return(ans % 10003)
+
 
 
 
