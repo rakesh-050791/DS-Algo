8fa8e2a939431ab8e6c4c7c0fe788718a58d68a8
rakesh050791 <rakesh050791@gmail.com>
2022-07-15 17:33:41 +0200

    Even subarrays

--- Basics/July-2022/13-July-2022.py
+++ Basics/July-2022/13-July-2022.py
@@ -104,6 +104,21 @@ class Solution:
         return result
 
 
+# 5 :  Even Subarrays
+# You are given an integer array A.
+
+# Decide whether it is possible to divide the array into one or more subarrays of even length such that the first and last element of all subarrays will be even.
+
+# Return "YES" if it is possible; otherwise, return "NO" (without quotes).
+class Solution:
+    # @param A : list of integers
+    # @return a strings
+    def solve(self, A):
+        arrLength = len(A)        
+        if arrLength % 2 == 0 and A[0] % 2 == 0 and A[-1] % 2 == 0: return "YES"
+        return "NO"
+
+
 
 
 
