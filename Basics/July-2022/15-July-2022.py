2784dd6ca9d4784495dc4c43279ed92391a3de14
rakesh050791 <rakesh050791@gmail.com>
2022-07-16 23:59:16 +0200

    Sum of All Subarrays

--- Basics/July-2022/15-July-2022.py
+++ Basics/July-2022/15-July-2022.py
@@ -18,3 +18,25 @@ class Solution:
                 sumOfEachSubarray = 0
                 
         return maxSum
+
+
+# 2 : Sum of All Subarrays
+
+
+# You are given an integer array A of length N.
+# You have to find the sum of all subarray sums of A.
+# More formally, a subarray is defined as a contiguous part of an array which we can obtain by deleting zero or more elements from either end of the array.
+# A subarray sum denotes the sum of all the elements of that subarray.
+
+class Solution:
+    # @param A : list of integers
+     # @return an long
+    def subarraySum(self, A):
+        sumOfSubArraySum = 0
+        arrLen = len(A)
+
+        for i in range(arrLen):
+            subArraySum = (A[i] * (i+1) * (arrLen - i))
+            sumOfSubArraySum += subArraySum
+        
+        return sumOfSubArraySum
