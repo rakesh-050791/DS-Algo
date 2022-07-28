7948a65c41438b17fc98dbdeaabb596b574d8ebd
rakesh050791 <rakesh050791@gmail.com>
2022-07-28 21:35:42 +0200

    Interesting Array

--- Basics/July-2022/22-July-2022.py
+++ Basics/July-2022/22-July-2022.py
@@ -73,5 +73,27 @@ class Solution:
         return count
 
 
+# 4 : Interesting Array 
+# You have an array A with N elements. We have two types of operation available on this array :
+# We can split an element B into two elements, C and D, such that B = C + D.
+# We can merge two elements, P and Q, to one element, R, such that R = P ^ Q i.e., XOR of P and Q.
+# You have to determine whether it is possible to convert array A to size 1, containing a single element equal to 0 after several splits and/or merge?
+
+class Solution:
+    # @param A : list of integers
+    # @return a strings
+    def solve(self, A):
+        count = 0 
+
+        for i in range(len(A)):
+            if A[i] % 2 != 0:
+                count += 1
+        
+
+        return 'Yes' if count % 2 == 0 else 'No'      
+
+
+
+
 
 
