da20ff449147ec441f2ee4ced86f9fdbfbf2c9be
rakesh050791 <rakesh050791@gmail.com>
2022-07-29 22:53:56 +0200

    A, B and Modulo

---
+++ Basics/July-2022/29-july-2022.py
@@ -0,0 +1,12 @@
+# 1 : A, B and Modulo
+# Given two integers A and B, find the greatest possible positive integer M, such that A % M = B % M.
+
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        return abs(A-B)
+
+
+# 2 : 
