fbdc1d054c10a5855778c37a5d814e398b6e04ec
rakesh050791 <rakesh050791@gmail.com>
2022-08-15 11:34:01 +0200

    Finding Good Days: another problem on counting set bits

--- Intermediate/July-2022/25-July-2022.py
+++ Intermediate/July-2022/25-July-2022.py
@@ -54,5 +54,60 @@ class Solution:
             A = A >> 1
         return numberOfSetBits
 
-# 2 :
+
+# 2 : Finding Good Days
+
+# Alex has a cat named Boomer. He decides to put his cat to the test for eternity.
+
+# He starts on day 1 with a stash of food unit, every next day, the stash doubles.
+
+# If Boomer is well behaved during a particular day, she receives food worth equal to the stash on that day.
+
+# Boomer receives a net worth of A units of food. What is the number of days he was well behaved?
+
+# Explanation :
+# Given stash doubles every next day:
+#   1st Day   stash is  20  =>  1 Unit
+#   2nd Day  stash is 21  =>  2 Units
+#   3rd Day  stash is  22   =>  4 Units
+#   4th Day  stash is  23   =>  8 Units
+#   5th Day  stash is  24   =>  16 Units
+#   . .
+#   . .
+#   Nth Day  stash is 2N-1 => say(X Units)
+
+# Cat will be rewarded with total stash units on that particular day if behaved properly, Else it won't get any food(i.e., 0 stash Units) if not behaved properly.
+ 
+# Ex1:
+#   now let's consider cat was given stash Units N = 13
+#   so, In what all ways we can add the above stash Units, such that total will be equal to N = 13.
+#   ==> (1 + 4 + 8) (i.e., 1st Day + 3rd Day + 4th Day) ==> 3 Days(Cat Behaved)
+#   Here we can write 13 in Binary as  '1101' and our answer is 3.
+  
+# Ex2:
+#   now let's consider cat was given stash Units N = 16
+#   so, In what all ways we can add the above stash Units, such that total will be equal to N = 16.
+#   ==> (16) (i.e., Only on 5th Day) ==> 1 Day (Cat Behaved)
+#   Here we can write 16 in Binary as  '10000' and our answer is 1.
+  
+# Ex3:
+# now let's consider cat was given stash Units N = 15
+#         so, In what all ways we can add the above stash Units, such that total will be equal to N = 15.
+# ==> (1 + 2 + 4 + 8) (i.e., 1st Day + 2nd Day + 3rd Day + 4th Day) ==> 4 Days (Cat Behaved)
+# Here we can write 15 in Binary as  '1111' and our answer is 4.
+ 
+# From Above Examples, we can observe whatever N can be, the number of Days cat behaved = number of Set bits in N.
+
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def solve(self, A):
+        numberOfSetBits = 0
+
+        while (A > 0):
+            if (A & 1 == 1):
+                numberOfSetBits += 1
+            
+            A = A >> 1
+        return numberOfSetBits
 
