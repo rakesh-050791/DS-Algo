fa07ac76a043f0cd6f6db7047e3ea4371a805970
rakesh050791 <rakesh050791@gmail.com>
2022-08-15 20:37:50 +0200

    logic updated for Alternating Subarrays problem

--- Intermediate/July-2022/15-July-2022.py
+++ Intermediate/July-2022/15-July-2022.py
@@ -160,7 +160,8 @@ class Solution:
 
 # You have to tell all the indices of array A that can act as a center of 2 * B + 1 length 0-1 alternating subarray.
 
-# A 0-1 alternating array is an array containing only 0's & 1's, and having no adjacent 0's or 1's. For e.g. arrays [0, 1, 0, 1], [1, 0] and [1] are 0-1 alternating, while [1, 1] and [0, 1, 0, 0, 1] are not.
+# A 0-1 alternating array is an array containing only 0's & 1's, and having no adjacent 0's or 1's. 
+# For e.g. arrays [0, 1, 0, 1], [1, 0] and [1] are 0-1 alternating, while [1, 1] and [0, 1, 0, 0, 1] are not.
 
 class Solution:
     # @param A : list of integers
@@ -173,15 +174,17 @@ class Solution:
         noOfSubArrays = n-subArrayLen+1
         for i in range(0, noOfSubArrays):
             prev = -1
-            flag = 1
+            is_alternating = 1
             for j in range(i, i+subArrayLen):
                 if A[j] == prev:
-                    flag = 0
+                    is_alternating = 0
                     break;
                 prev = A[j]
-            if flag == 1:
+            if is_alternating == 1:
                 result.append(i+B)
         return result
+
+
 
 
 
