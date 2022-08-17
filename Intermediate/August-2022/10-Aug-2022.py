d9e6eccb095aa4d58eaebce7803820d3a49c9815
rakesh050791 <rakesh050791@gmail.com>
2022-08-17 23:10:05 +0200

    Target number or 2 sum problem

--- Intermediate/August-2022/10-Aug-2022.py
+++ Intermediate/August-2022/10-Aug-2022.py
@@ -85,3 +85,31 @@ class Solution:
                     return 1
         return 0
 
+# 3 : 2 Sum
+# Given an array of integers, find two numbers such that they add up to a specific target number.
+
+# The function twoSum should return indices of the two numbers such that they add up to the target, where index1 < index2. Please note that your returned answers (both index1 and index2 ) are not zero-based. Put both these numbers in order in an array and return the array from your function ( Looking at the function signature will make things clearer ). Note that, if no pair exists, return empty list.
+
+# If multiple solutions exist, output the one where index2 is minimum. If there are multiple solutions with the minimum index2, choose the one with minimum index1 out of them.
+
+# Input: [2, 7, 11, 15], target=9
+# Output: index1 = 1, index2 = 2
+
+class Solution:
+    # @param A : tuple of integers
+    # @param B : integer
+    # @return a list of integers
+    def twoSum(self, A, B):
+        n = len(A)
+        k = B
+        frequencyMap = {}
+        for i in range(n):
+            a = A[i]
+            target = k - a
+
+            if target in frequencyMap:
+                return frequencyMap[target], i+1
+            elif a not in frequencyMap:
+                frequencyMap[a] = i+1 
+        return []
+
