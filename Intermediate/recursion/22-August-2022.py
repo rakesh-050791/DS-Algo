7ebab2a8455870959ccf0b7c089a3e519411bb35
rakesh050791 <rakesh050791@gmail.com>
2022-08-23 00:21:51 +0200

    implemented power function using recursion

--- Intermediate/recursion/22-August-2022.py
+++ Intermediate/recursion/22-August-2022.py
@@ -15,3 +15,33 @@ def digitsSum(A):
     remainingDigits = A // 10
 
     return (lastDigit + digitsSum(remainingDigits))
+
+
+# 2 : Implement Power Function
+
+# Implement pow(A, B) % C.
+# In other words, given A, B and C, Find (AB % C).
+
+# Note: The remainders on division cannot be negative. In other words, make sure the answer you return is non-negative.
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @param C : integer
+    # @return an integer
+    def pow(self, A, B, C):
+        return calculateExponent(A,B,C)
+
+def calculateExponent(A,B,C):
+    if B == 0:
+        return 1 % C
+
+    if B == 1:
+        return A % C
+
+    exponent = (calculateExponent(A, B//2, C)) % C
+
+    if B % 2 == 0:
+        return (exponent * exponent) % C
+    else:
+        return ((exponent * exponent) % C * A ) % C
+
