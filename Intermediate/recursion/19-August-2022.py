f9bced0dbb22ef0a7b8625ca2e903aee64b79143
rakesh050791 <rakesh050791@gmail.com>
2022-08-24 00:17:43 +0200

    Find Fibonacci using recursion

--- Intermediate/recursion/19-August-2022.py
+++ Intermediate/recursion/19-August-2022.py
@@ -52,18 +52,32 @@ def isPalindrome(s, i, j):
 # print(result)
 
 
-# 3 : Find Factorial!
-# Write a program to find the factorial of the given number A.
+# 3 : Find Fibonacci - II
+# The Fibonacci numbers are the numbers in the following integer sequence.
+
+# 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, ……..
+
+# In mathematical terms, the sequence Fn of Fibonacci numbers is defined by the recurrence relation:
+
+# Fn = Fn-1 + Fn-2
+
+# Given a number A, find and return the Ath Fibonacci Number.
+
+# Given that F0 = 0 and F1 = 1.
 
 class Solution:
     # @param A : integer
     # @return an integer
-    def solve(self, A):
-        return factorial(A)
+    def findAthFibonacci(self, A):
 
-def factorial(n):
+        return fibSeries(A)
+    
+
+def fibSeries(n):
     if n == 0:
+        return 0
+    if n == 1:
         return 1
 
-    return factorial(n-1) * n
+    return fibSeries(n - 1) + fibSeries(n - 2)
     
