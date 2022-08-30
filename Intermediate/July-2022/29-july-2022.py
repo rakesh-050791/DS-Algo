f085c6a43554dabc6999a55d3319fddf62e794d5
rakesh050791 <rakesh050791@gmail.com>
2022-08-31 00:49:19 +0200

    Modular Arithmetic Introduction | Concatenate Three Numbers

--- Intermediate/July-2022/29-july-2022.py
+++ Intermediate/July-2022/29-july-2022.py
@@ -9,4 +9,14 @@ class Solution:
         return abs(A-B)
 
 
-# 2 : 
+# 2 : Concatenate Three Numbers
+# Given three 2-digit integers, A, B, and C, find out the minimum number obtained by concatenating them in any order.
+
+# Return the minimum result obtained.
+class Solution:
+    def solve(self, A, B, C):
+        minimumElement = min(A, B, C)
+        maximumElement = max(A, B, C)
+        middleElement = A + B + C - maximumElement - minimumElement
+        
+        return (minimumElement*10000 + middleElement*100 + maximumElement)
