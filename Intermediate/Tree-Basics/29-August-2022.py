342d34265fc5b519ba98a1074dc1ddae5c16897a
rakesh050791 <rakesh050791@gmail.com>
2022-09-02 20:09:55 +0200

    Tree Basics | Inorder traversal

--- Intermediate/Tree-Basics/29-August-2022.py
+++ Intermediate/Tree-Basics/29-August-2022.py
@@ -23,3 +23,35 @@ class Solution:
         return 1 + max(left, right)
 
 
+# 2 :  Inorder Traversal
+# Given a binary tree, return the inorder traversal of its nodes values.
+# Input :
+# 	1
+#   / \
+#  6   2
+#     /
+#    3
+# Output :  [6, 1, 3, 2]
+
+import sys
+sys.setrecursionlimit(10**6)
+
+class Solution:
+	# @param A : root node of tree
+	# @return a list of integers
+	def inorderTraversal(self, A):
+        result = []
+        self.inOrder(A, result)
+        return result
+        
+    def inOrder(self, root, result):
+        if root == None:
+            return
+
+        if root.left:
+            self.inOrder(root.left, result)
+            
+        result.append(root.val)            
+
+        if root.right:
+            self.inOrder(root.right, result)
