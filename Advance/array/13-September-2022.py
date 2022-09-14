33bdebb38f0ae875d0acc929f0b39ebc112bb551
rakesh050791 <rakesh050791@gmail.com>
2022-09-15 00:37:55 +0200

    Arrays | Sub-matrix Sum Queries

--- Advance/array/13-September-2022.py
+++ Advance/array/13-September-2022.py
@@ -35,6 +35,103 @@ class Solution:
         
         return sumOfSubMatrices
 
+
+# 2 : Sub-matrix Sum Queries
+
+# Given a matrix of integers A of size N x M and multiple queries Q, for each query, find and return the submatrix sum.
+
+# Inputs to queries are top left (b, c) and bottom right (d, e) indexes of submatrix whose sum is to find out.
+
+# NOTE:
+
+# Rows are numbered from top to bottom, and columns are numbered from left to right.
+# Sum may be large, so return the answer mod 109 + 7.
  
+
+# Example Input
+# Input 1:
+
+#  A = [   [1, 2, 3]
+#          [4, 5, 6]
+#          [7, 8, 9]   ]
+#  B = [1, 2]
+#  C = [1, 2]
+#  D = [2, 3]
+#  E = [2, 3]
+
+# Example Output
+# Output 1:
+
+#  [12, 28]
+
+# Example Explanation
+# Explanation 1:
+
+#  For query 1: Submatrix contains elements: 1, 2, 4 and 5. So, their sum is 12.
+#  For query 2: Submatrix contains elements: 5, 6, 8 and 9. So, their sum is 28.
+
+class Solution:
+    # @param A : list of list of integers
+    # @param B : list of integers
+    # @param C : list of integers
+    # @param D : list of integers
+    # @param E : list of integers
+    # @return a list of integers
+    def solve(self, A, B, C, D, E):
+        rows = len(A)
+        cols = len(A[0])
+        prefixSum = [[0] * cols for i in range(rows)]
+
+        # calculating rowwise prefixSum 
+        for i in range(rows):
+            sums = 0
+            for j in range(cols):
+                sums += A[i][j]
+                prefixSum[i][j] = sums
+
+        # calculating colwise prefixSum 
+        for j in range(cols):
+            sums = 0
+            for i in range(rows):
+                sums += prefixSum[i][j]
+                prefixSum[i][j] = sums
+
+
+        # subMatrix Sum calculation 
+        result = []
+
+        for i in range(len(B)):
+            a1, b1 = B[i] - 1 , C[i] - 1 ## TopLeft corners
+            a2, b2 = D[i] - 1 , E[i] - 1 ## BottomRight corners
+
+            totalSum = prefixSum[a2][b2] ##(BottomRight prefixSum)
+
+            # removing extra subArrays 
+
+            ## removing left part
+            if a1 > 0:
+                totalSum -= prefixSum[a1 - 1][b2]
+
+            ## removing top part 
+            if b1 > 0:
+                totalSum -= prefixSum[a2][b1 - 1]
+
+            ## neutralizing common part 
+            if a1 > 0 and b1 > 0:
+                totalSum += prefixSum[a1 - 1][b1 - 1]
+
+            result.append(totalSum % (10**9 + 7))
+
+        return result
+            
+
+
+
+
+
+
+
+
+
 
 
