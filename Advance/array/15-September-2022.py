991c1b581c90ef6cf089c68283607e7c76ab55c1
rakesh050791 <rakesh050791@gmail.com>
2022-09-18 18:05:26 +0200

    Arrays | minimum swaps

--- Advance/array/15-September-2022.py
+++ Advance/array/15-September-2022.py
@@ -9,9 +9,7 @@
 # Given intervals [1, 3], [6, 9] insert and merge [2, 5] .
 
 # Example Output
-# Output 1:
-
-#  [ [1, 5], [6, 9] ]
+# Output 1: [ [1, 5], [6, 9] ]
 
 # Definition for an interval.
 # class Interval:
@@ -44,7 +42,58 @@ class Solution:
 
         return result
 
-# 2 : Minimum Swaps 2
+# 2 : Minimum Swaps
+# Given an array of integers A and an integer B, find and return the minimum number of swaps 
+# required to bring all the numbers less than or equal to B together.
+# Note: It is possible to swap any two elements, not necessarily consecutive.
+
+# Example Input
+
+# Input 1:
+
+#  A = [1, 12, 10, 3, 14, 10, 5]
+#  B = 8
+
+#  Example Output
+
+# Output 1: 2
+
+# Explanation 1:
+
+#  A = [1, 12, 10, 3, 14, 10, 5]
+#  After swapping  12 and 3, A => [1, 3, 10, 12, 14, 10, 5].
+#  After swapping  the first occurence of 10 and 5, A => [1, 3, 5, 12, 14, 10, 10].
+#  Now, all elements less than or equal to 8 are together.
+
+class Solution:
+    def solve(self, A, B):
+        n = len(A)
+        noOfSwaps = 0
+        result = float('inf')
+        noOfElementsLessthenB = 0 
+
+        for element in A:
+            if element <= B:
+                noOfElementsLessthenB += 1
+
+        for i in range(noOfElementsLessthenB):
+            if A[i] > B:
+                noOfSwaps += 1
+
+        result = min(result, noOfSwaps)
+
+        for i in range(noOfElementsLessthenB, n):
+            if A[i-noOfElementsLessthenB] <=B and A[i] > B:
+                noOfSwaps += 1
+            elif A[i-noOfElementsLessthenB] > B and A[i] <=B:
+                noOfSwaps -= 1
+            result = min(result, noOfSwaps)
+
+        return result
+
+
+
+# 3 : Minimum Swaps 2
 
 # Given an array of integers A of size N that is a permutation of [0, 1, 2, ..., (N-1)]. It is allowed to 
 # swap any two elements (not necessarily consecutive).
@@ -56,9 +105,7 @@ class Solution:
 # A = [1, 2, 3, 4, 0]
 
 # Example Output
-# Output 1:
-
-# 4
+# Output 1: 4
 
 # Example Explanation
 # Explanation 1:
@@ -81,4 +128,4 @@ class Solution:
                 A[num] = temp
                 noOfSwaps += 1
         return noOfSwaps
-
+        
