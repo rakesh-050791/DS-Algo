b5d1debb25404e1e6a7dca576973798edd8ae26d
rakesh050791 <rakesh050791@gmail.com>
2022-09-22 15:00:23 +0200

    Bit-Manipulation | Count Number of 1 Bits

--- Advance/Bit-Manipulations/19-September-2022.py
+++ Advance/Bit-Manipulations/19-September-2022.py
@@ -22,5 +22,27 @@
         return result
 
 
-# 2 : 
+# 2 : Number of 1 Bits
+# Write a function that takes an integer and returns the number of 1 bits it has.
 
+# Example Input
+# Input1:
+# 11
+
+# Example Output
+# Output1:
+# 3
+
+# Explaination1:
+# 11 is represented as 1011 in binary.
+
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def numSetBits(self, A):
+        totalOneBits = 0
+        for i in range(32):
+           if ( (A >> i) & 1) == True:
+               totalOneBits += 1
+        
+        return totalOneBits
