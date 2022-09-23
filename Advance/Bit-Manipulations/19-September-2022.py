1f0477b61f8cdaa662115a6b7e76381a2ee03ad9
rakesh050791 <rakesh050791@gmail.com>
2022-09-23 09:38:50 +0200

    Bit-Manipulations | Single Number II

--- Advance/Bit-Manipulations/19-September-2022.py
+++ Advance/Bit-Manipulations/19-September-2022.py
@@ -46,3 +46,39 @@ class Solution:
                totalOneBits += 1
         
         return totalOneBits
+
+
+# 3 : Single Number II
+
+# Problem Description
+# Given an array of integers, every element appears thrice except for one, which occurs once.
+
+# Find that element that does not appear thrice.
+
+# NOTE: Your algorithm should have a linear runtime complexity.
+
+# Could you implement it without using extra memory?
+
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def singleNumber(self, A):
+        result = 0
+
+        for i in range(32):
+            ci = 0 # For ith bit calculate number of ar[] with ith bit set ?
+            for j in range(len(A)):
+                if self.checkBit(A[j], i) == True:
+                    ci += 1
+
+            if (ci % 3 != 0): # Checking if for unique element ith bit is set ?
+                result += (1 << i) # OR 2 raise to power i
+        return result
+
+
+    ## Below is the method that will tell if bit is 
+    ## set or unset at particular position or any binary number
+    
+    def checkBit(self, element, position):
+        
+        return (element >> position) & 1 
