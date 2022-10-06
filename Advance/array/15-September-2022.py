a7eb48a29c0f8ef6d33be02420bb7de15e1c7a67
rakesh050791 <rakesh050791@gmail.com>
2022-10-07 00:21:21 +0200

    Arrays | Maximum Absolute Difference

--- Advance/array/15-September-2022.py
+++ Advance/array/15-September-2022.py
@@ -203,6 +203,8 @@ class Solution:
 
 #  Row 0 has maximum number of 1s.
 
+## Youtube explanation : https://www.youtube.com/watch?v=1iSJhfje97I
+
 class Solution:
     # @param A : list of list of integers
     # @return an integer
@@ -218,3 +220,54 @@ class Solution:
                     row = i
                 col -= 1
         return row
+
+
+# 6 : Maximum Absolute Difference
+
+# You are given an array of N integers, A1, A2, .... AN.
+
+# Return the maximum value of f(i, j) for all 1 ≤ i, j ≤ N. f(i, j) is defined as |A[i] - A[j]| + |i - j|, 
+# where |x| denotes absolute value of x.
+
+# Input 1: A = [1, 3, -1]
+
+# Output 1: 5
+
+# Explanation 1:
+
+# f(1, 1) = f(2, 2) = f(3, 3) = 0
+# f(1, 2) = f(2, 1) = |1 - 3| + |1 - 2| = 3
+# f(1, 3) = f(3, 1) = |1 - (-1)| + |1 - 3| = 4
+# f(2, 3) = f(3, 2) = |3 - (-1)| + |2 - 3| = 5
+
+# So, we return 5.
+
+# Video Explanation : https://www.youtube.com/watch?v=tN8wEDNZKF4 
+
+class Solution:
+    def maxArr(self, A):
+        # We have to find |A[i] - A[j]| + |i - j|
+        # for mod we can get four possiblities (+, +) , (+ , -), (- , +), (-, -)
+
+        # So we can write the above as
+        # (A[i] - A[j]) + (i - j) => (A[i] + i) - (A[j] + j)
+        # (A[i] - A[j]) - (i - j) => (A[i] - i) - (A[j] - j)
+        # -(A[i] - A[j]) + (i - j) => -(A[i] - i) + (A[j] - j)
+        # -(A[i] - A[j]) - (i - j) => -(A[i] + i) + (A[j] + j)
+
+        # So from the above we can see that we have to find only four pair
+        # A = A[i] + i = maxForI
+        # B = A[i] - i = minForI
+        # C = A[j] + j = maxForJ
+        # D = A[j] - j = minForJ
+
+        maxForI=A[0]
+        maxForJ=A[0]
+        minForI=A[0]
+        minForJ=A[0]
+        for i in range(len(A)):
+            maxForI=max(maxForI,A[i]+i)
+            maxForJ=max(maxForJ,A[i]-i)
+            minForI=min(minForI,A[i]+i)
+            minForJ=min(minForJ,A[i]-i)
+        return max(maxForI-minForI, maxForJ-minForJ)
