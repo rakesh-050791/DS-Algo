0cd7e54a4e17a0ff6c849ea634452d1aeff0cb9b
rakesh050791 <rakesh050791@gmail.com>
2022-10-13 23:05:14 +0200

    Hashing | Colorful Number

--- Intermediate/August-2022/08-Aug-2022.py
+++ Intermediate/August-2022/08-Aug-2022.py
@@ -184,3 +184,71 @@ class Solution:
             return -1
         else:
             return minDiff
+
+# 6 : Colorful Number
+# Given a number A, find if it is COLORFUL number or not.
+
+# If number A is a COLORFUL number return 1 else, return 0.
+
+# What is a COLORFUL Number:
+
+# A number can be broken into different contiguous sub-subsequence parts. 
+# Suppose, a number 3245 can be broken into parts like 3 2 4 5 32 24 45 324 245. 
+# And this number is a COLORFUL number, since product of every digit of a contiguous subsequence is different.
+
+# Example Input
+# Input 1: A = 23 , Input 2: A = 236
+
+# Example Output
+# Output 1: 1, Output 2: 0
+
+# Example Explanation
+# Explanation 1:
+
+#  Possible Sub-sequences: [2, 3, 23] where
+#  2 -> 2 
+#  3 -> 3
+#  23 -> 6  (product of digits)
+#  This number is a COLORFUL number since product of every digit of a sub-sequence are different. 
+# Explanation 2:
+
+#  Possible Sub-sequences: [2, 3, 6, 23, 36, 236] where
+#  2 -> 2 
+#  3 -> 3
+#  6 -> 6
+#  23 -> 6  (product of digits)
+#  36 -> 18  (product of digits)
+#  236 -> 36  (product of digits)
+#  This number is not a COLORFUL number since the product sequence 23  and sequence 6 is same.
+
+class Solution:
+    def colorful(self, A):
+        digitsList = [] #  Split A into list  example A = 236 -> [2,3,6]
+        while A :
+            digitsList.append(A % 10)
+            A = A // 10
+        
+        digitsList = list(reversed(digitsList))
+        
+        n = len(digitsList)
+        digitsProductSet = set() #Declared set to store product of digitslist
+
+        for i in range(n):
+            product = 1
+            for j in range(i, n):
+                product *= digitsList[j]
+
+                if product in digitsProductSet:
+                    return 0
+                else:
+                    digitsProductSet.add(product)
+        return 1
+
+
+
+
+
+
+
+
+
