4ad276d152faa91dd47a7507f210205790b0a258
rakesh050791 <rakesh050791@gmail.com>
2022-10-14 19:27:28 +0200

    Hashing | K Occurrences

--- Intermediate/August-2022/08-Aug-2022.py
+++ Intermediate/August-2022/08-Aug-2022.py
@@ -245,10 +245,58 @@ class Solution:
         return 1
 
 
+# 7 : K Occurrences
 
+# Groot has N trees lined up in front of him where the height of the i'th tree is denoted by H[i].
 
+# He wants to select some trees to replace his broken branches.
 
+# But he wants uniformity in his selection of trees.
 
+# So he picks only those trees whose heights have frequency B.
 
+# He then sums up the heights that occur B times. (He adds the height only once to the sum and not B times).
+
+# But the sum he ended up getting was huge so he prints it modulo 10^9+7.
+
+# In case no such cluster exists, Groot becomes sad and prints -1.
+
+# Input: Integers N and B and an array C of size N
+
+# Output: Sum of all numbers in the array that occur exactly B times.
+
+# Example:
+
+# Input:
+
+# N=5 ,B=2 ,C=[1 2 2 3 3]
+# Output:
+
+# 5
+# Explanation:
+
+# There are 3 distinct numbers in the array which are 1,2,3.
+# Out of these, only 2 and 3 occur twice. Therefore the answer is sum of 2 and 3 which is 5.
+
+class Solution:
+    def getSum(self, A, B, C):
+        hashMap = {}
+        for i in C:
+            if i in hashMap:
+                hashMap[i] += 1
+            else:
+                hashMap[i] = 1
+
+        sum = 0
+        flag = 0 # Maintaning flag to track if all elements in hashmap have frequency 1
+        for i in hashMap:
+            if hashMap[i] == B:
+                sum += i
+                flag = 1
+
+        if flag == 1:
+            return sum % (10**9 + 7)
+        else :
+            return -1
 
 
