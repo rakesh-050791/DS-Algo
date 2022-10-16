9615365353cf0aed2b0dc3dcded5e1b5cc6d5b1a
rakesh050791 <rakesh050791@gmail.com>
2022-10-16 20:55:18 +0200

    Strings | Isalpha()

--- Intermediate/August-2022/05-Aug-2022.py
+++ Intermediate/August-2022/05-Aug-2022.py
@@ -150,3 +150,41 @@ class Solution:
         return 0 if nonAlphanumericCount > 0 else 1
 
 
+# 7 : Isalpha()
+
+# You are given a function isalpha() consisting of a character array A.
+
+# Return 1 if all the characters of the character array are alphabetical (a-z and A-Z), else return 0.
+
+# Output Format
+
+# Return 1 if all the characters of the character array are alphabetical (a-z and A-Z), else return 0.
+
+# Example Input
+
+# Input 1:
+
+# A = ['S', 'c', 'a', 'l', 'e', 'r', 'A', 'c', 'a', 'd', 'e', 'm', 'y']
+
+# Example Output
+
+# Output 1: 1
+
+# Explanation 1:
+
+# All the characters are alphabets.
+
+class Solution:
+    # @param A : list of characters
+    # @return an integer
+    def solve(self, A):
+        nonAlphanumericCount = 0
+        for i in range(len(A)):
+            if (ord(A[i]) >= 65 and ord(A[i]) <= 90) or (ord(A[i]) >= 97 and ord(A[i]) <= 122):
+                pass
+            else:
+                nonAlphanumericCount += 1
+        
+        return 0 if nonAlphanumericCount > 0 else 1
+
+
