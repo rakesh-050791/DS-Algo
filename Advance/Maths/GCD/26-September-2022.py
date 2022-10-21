66492092b3caf1f3cbc438b9d2b7af7991f37362
rakesh050791 <rakesh050791@gmail.com>
2022-10-21 22:44:29 +0200

    Modular Arithmetic | GCD | Find Greatest Common Divisor

--- Advance/Maths/GCD/26-September-2022.py
+++ Advance/Maths/GCD/26-September-2022.py
@@ -148,3 +148,35 @@ class Solution:
                 return 1 #if 1 is health then its always 1
         
         return previousElement
+
+
+# 4 : Greatest Common Divisor
+
+# Given 2 non-negative integers A and B, find gcd(A, B)
+
+# GCD of 2 integers A and B is defined as the greatest integer 'g' such that 'g' is a divisor of both A and B. Both A and B fit in a 32 bit signed integer.
+
+# Note: DO NOT USE LIBRARY FUNCTIONS.
+
+
+# Example Input
+# Input 1:
+
+# A = 4, B = 6
+
+# Example Output
+# Output 1: 2
+
+# Example Explanation
+# Explanation 1: 2 divides both 4 and 6
+
+class Solution:
+    def gcd(self, A, B):
+        a = abs(A)
+        b = abs(B)
+        
+        if b == 0:
+            return a
+
+        return self.gcd(b, a % b)
+    
