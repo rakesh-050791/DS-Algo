cafce9758af2026ed5d9008b4c0a064b27bce33a
rakesh050791 <rakesh050791@gmail.com>
2022-10-25 20:33:24 +0200

    Binary Search | Aggressive cows

--- Advance/Binary-Search/19-October-2022.py
+++ Advance/Binary-Search/19-October-2022.py
@@ -74,7 +74,77 @@ class Solution:
                 if count == painters:
                     return False
         return True
+
+
+# 2 : Aggressive cows
+
+# Farmer John has built a new long barn with N stalls. Given an array of integers A of size N where each element of the array represents the location of the stall and an integer B which represents the number of cows.
+
+# His cows don't like this barn layout and become aggressive towards each other once put into a stall. To prevent the cows from hurting each other, John wants to assign the cows to the stalls, such that the minimum distance between any two of them is as large as possible. What is the largest minimum distance?
+
+
+# Input Format
+# The first argument given is the integer array A.
+# The second argument given is the integer B.
+
+
+# Output Format
+# Return the largest minimum distance possible among the cows.
+
+# Example Input
+# Input 1:
+
+# A = [1, 2, 3, 4, 5]
+# B = 3
+
+# Example Output
+# Output 1: 2
+
+# Example Explanation
+# Explanation 1:
+
+# John can assign the stalls at location 1, 3 and 5 to the 3 cows respectively. So the minimum distance will be 2.
+
+class Solution:
+    def solve(self, A, B):
+        A.sort()
+        return self.binarySearch(A, B)
         
+    
+    def binarySearch(self, stalls, cows):
+        n = len(stalls)
+        low = 1
+        high = stalls[n-1] - stalls[0]
+        result = 0
+
+        while (low <= high):
+            mid = (low + high) // 2
+            
+            if (self.check(stalls, cows, mid) == True):
+                result = mid #if we can keep cows atleast mid distance apart
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        return result
+    
+
+    def check(self, stalls, totalCows, mid):
+        n = len(stalls)
+        lastCowPlaced = stalls[0]
+        cowsCount = 1
+
+        for i in range(1, n):
+            if (stalls[i] - lastCowPlaced) >= mid:
+                lastCowPlaced = stalls[i]
+                cowsCount += 1
+
+                if cowsCount == totalCows:
+                    return True
+        
+        return False
+
+
 
 # 3 : Allocate Books
 
