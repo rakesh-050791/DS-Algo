a9a53eee67ccb579e6fda9bb39d2a5203b96323f
rakesh050791 <rakesh050791@gmail.com>
2022-10-28 02:15:19 +0200

    Binary Search | Search in Bitonic Array

--- Advance/Binary-Search/13-October-2022.py
+++ Advance/Binary-Search/13-October-2022.py
@@ -48,3 +48,72 @@ class Solution:
         return arr[low]
             
 
+# 4 : Search in Bitonic Array!
+
+# Given a bitonic sequence A of N distinct elements, write a program to find a given element B in the bitonic sequence in O(logN) time.
+
+# NOTE:
+
+# A Bitonic Sequence is a sequence of numbers which is first strictly increasing then after a point strictly decreasing.
+
+
+# Example Input
+# Input 1:
+
+#  A = [3, 9, 10, 20, 17, 5, 1]
+#  B = 20
+
+# Example Output
+# Output 1: 3
+ 
+# Example Explanation
+# Explanation 1: B = 20 present in A at index 3
+
+class Solution:
+    def solve(self, A, B):
+        n = len(A)
+
+        #Searching Bitonic element
+        low = 0
+        high = n-1
+        while (low <= high):
+            mid = (low + high) // 2
+
+            if (A[mid-1] < A[mid]) and (A[mid] > A[mid+1]):
+                bionicPoint = mid
+                break
+
+            elif A[mid+1] > A[mid]:
+                low = mid + 1
+            elif A[mid-1] > A[mid]:
+                high = mid - 1
+
+        # Binary search on the left side of the Bitonic element
+        low = 0
+        high = bionicPoint-1
+        while (low <= high):
+            mid = (low + high) // 2
+
+            if A[mid] == B:
+                return mid
+            
+            elif A[mid] < B:
+                low = mid + 1
+            else:
+                high = mid - 1
+
+        # Binary search on the right side of the Bitonic element
+        low = bionicPoint
+        high = n-1
+        while (low <= high):
+            mid = (low + high) // 2
+
+            if A[mid] == B:
+                return mid
+            
+            elif A[mid] < B:
+                high = mid - 1
+            else:
+                 low = mid + 1
+
+        return -1
