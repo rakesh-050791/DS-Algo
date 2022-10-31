1812355ec9fee1f3daf730588b692a94af4d9b24
rakesh050791 <rakesh050791@gmail.com>
2022-11-01 00:18:09 +0100

    Hashing | Longest Consecutive Sequence

--- Advance/Hashing/31-October-2022.py
+++ Advance/Hashing/31-October-2022.py
@@ -39,3 +39,74 @@ class Solution:
                 i += 1
 
         return longestSubString
+
+
+# 2 : Longest Consecutive Sequence
+
+# Given an unsorted integer array A of size N.
+
+# Find the length of the longest set of consecutive elements from array A.
+
+
+# Example Input
+
+# Input 1:
+
+# A = [100, 4, 200, 1, 3, 2]
+
+# Example Output
+
+# Output 1: 4
+ 
+# Example Explanation
+
+# Explanation 1:
+
+# The set of consecutive elements will be [1, 2, 3, 4].
+
+
+# Approach 1
+
+class Solution:
+
+    # check x-1 in hashSet , if not then start if that element and check for x+1 elements maintain count and return max(count)
+    def longestConsecutive(self, A):
+
+        hashSet = set(A) #creating set out of a given array
+        longestSetLength = 0
+
+        for x in hashSet:
+            previousElement = x - 1
+
+            if previousElement not in hashSet:
+                length = 0
+                while x in hashSet:
+                    length += 1
+                    x += 1
+                longestSetLength = max(longestSetLength, length)
+
+        return longestSetLength
+    
+
+# Approach 2
+
+class Solution:
+
+    def longestConsecutive(self, A):
+
+        hashSet = set(A) #creating set out of a given array
+        longestSetLength = 0
+
+        for x in hashSet:
+            previousElement = x - 1
+
+            if previousElement in hashSet:
+                continue
+            else:
+                length = 0
+                while x in hashSet:
+                    x += 1
+                    length += 1
+                longestSetLength = max(longestSetLength, length)
+
+        return longestSetLength
