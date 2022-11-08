79f75f417da2acaa90866cd33cc75f64454df1a6
rakesh050791 <rakesh050791@gmail.com>
2022-11-08 01:28:10 +0100

    Strings-Pattern-Matching | Closest Palindrome

--- Advance/Strings-Pattern-Matching/04-November-2022.py
+++ Advance/Strings-Pattern-Matching/04-November-2022.py
@@ -168,3 +168,67 @@ class Solution:
                 break
 
         return outputStr + B[0]
+
+
+# 4 : Closest Palindrome
+
+# Groot has a profound love for palindrome which is why he keeps fooling around  with strings.
+# A palindrome string is one that reads the same backward as well as forward.
+
+# Given a string A of size N consisting of lowercase alphabets, he wants to know if it is possible to make the given string a palindrome by changing exactly one of its character.
+
+
+# Output Format
+# Return the string YES if it is possible to make the given string a palindrome by changing exactly 1 character. Else, it should return the string NO.
+
+
+
+# Example Input
+# Input 1:
+
+# A = "abbba"
+
+# Output 1: "YES"
+
+
+# Example Explanation
+# Explanation 1:
+
+#  We can change the character at index 3(1-based) to any other character. The string will be palindromic.
+
+
+# Solution Approach 
+
+# We apply our standard palindrome checking algorithm and count the number of times a set of mirror indices has different characters.
+# If at the end of processing, this count is greater than 1, then it can never be possible since we will have to change more than one character to make it a palindrome.
+# If the count is 1, the answer is always yes.
+# A corner case that needs to be considered is the case when the count is 0.
+# If the count is 0 and the palindrome length is even, then we cannot change exactly one character to make it a palindrome. We will have to change two mirror indices.
+# But if the count is 0 and the length is odd, then it is possible as we can change the middlemost character to anything.
+
+class Solution:
+    def solve(self, A):
+
+        n = len(A)
+        count = 0
+        
+        i , j = 0, n-1
+
+        while i < j:
+            if A[i] != A[j]:
+                count += 1
+
+            if count > 1:
+                return 'NO'                
+            
+            i += 1
+            j -= 1
+        
+        if count == 1:
+            return 'YES'
+
+        if count == 0:
+            if n % 2 == 0:
+                return 'NO'
+            else:
+                return 'YES'
