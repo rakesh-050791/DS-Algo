50b11298c7ca8024a099f49227fb34da31f8be62
rakesh050791 <rakesh050791@gmail.com>
2022-11-11 01:47:33 +0100

    Linked-List | Delete middle node

--- Advance/Linked-List/10-November-2022.py
+++ Advance/Linked-List/10-November-2022.py
@@ -90,3 +90,47 @@ def print_ll():
             currentNode = currentNode.next
         
         print(currentNode.data)
+
+
+# 2 : Delete middle node of a Linked List
+
+# Given a singly linked list, delete middle of the linked list.
+
+# For example, if given linked list is 1->2->3->4->5 then linked list should be modified to 1->2->4->5
+
+# If there are even nodes, then there would be two middle nodes, we need to delete the second middle element.
+
+# For example, if given linked list is 1->2->3->4->5->6 then it should be modified to 1->2->3->5->6.
+
+# Return the head of the linked list after removing the middle node.
+
+# If the input linked list has 1 node, then this node should be deleted and a null node should be returned.
+
+
+# Input Format
+
+# The only argument given is the node pointing to the head node of the linked list
+
+
+# Solution using slow, fast approach 
+
+class Solution:
+    def solve(self, A):
+        if not A or not A.next:
+            return None
+
+        slow = A 
+        fast = A
+        previous = None
+
+        # while(fast.next != None and fast.next.next != None):
+        while fast and fast.next:
+            previous = slow
+            slow = slow.next
+            fast = fast.next.next
+
+        previous.next = slow.next
+
+        return A
+
+
