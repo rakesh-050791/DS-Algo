881aea29b91ea8e587eca048fb6bd604be044987
rakesh050791 <rakesh050791@gmail.com>
2022-11-13 17:44:43 +0100

    Linked-List | K reverse linked list

--- Advance/Linked-List/10-November-2022.py
+++ Advance/Linked-List/10-November-2022.py
@@ -353,3 +353,98 @@ class Solution:
             temp.next = None #breaking link from existing linkedList 
             temp.next = headOfReverseLL #adding link to a new linkedList 
             headOfReverseLL = temp
+
+
+# 6 : K reverse linked list
+
+# Given a singly linked list A and an integer B, reverse the nodes of the list B at a time and return the modified linked list.
+
+
+
+# Problem Constraints
+# 1 <= |A| <= 103
+
+# B always divides A
+
+
+
+# Input Format
+# The first argument of input contains a pointer to the head of the linked list.
+
+# The second arugment of input contains the integer, B.
+
+
+
+# Output Format
+# Return a pointer to the head of the modified linked list.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [1, 2, 3, 4, 5, 6]
+#  B = 2
+# Input 2:
+
+#  A = [1, 2, 3, 4, 5, 6]
+#  B = 3
+
+
+# Example Output
+# Output 1:
+
+#  [2, 1, 4, 3, 6, 5]
+# Output 2:
+
+#  [3, 2, 1, 6, 5, 4]
+
+
+# Example Explanation
+# Explanation 1:
+
+#  For the first example, the list can be reversed in groups of 2.
+#     [[1, 2], [3, 4], [5, 6]]
+#  After reversing the K-linked list
+#     [[2, 1], [4, 3], [6, 5]]
+# Explanation 2:
+
+#  For the second example, the list can be reversed in groups of 3.
+#     [[1, 2, 3], [4, 5, 6]]
+#  After reversing the K-linked list
+#     [[3, 2, 1], [6, 5, 4]]
+
+sys.setrecursionlimit(10**6)
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def reverseList(self, A, B):
+        if not A:
+            return 
+
+        originalHead = A # preserving original head, so that we can link the initial B reversed elements with next b elements using recursion
+        head = A
+        temp = head
+        reverseListHead = None 
+        count = B
+
+        while (count > 0 and head != None):
+            count -= 1
+            temp = head
+            head = head.next
+            temp.next = None # breaking link from existing Linked ListNode
+            temp.next = reverseListHead
+            reverseListHead = temp
+
+        
+        newHead = self.reverseList(head, B) # newHead after B elements
+
+        originalHead.next = newHead
+
+        return reverseListHead
+
+        
