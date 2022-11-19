b2f4ba126e57f3545d73567e388ed4858a21690e
rakesh050791 <rakesh050791@gmail.com>
2022-11-19 01:15:58 +0100

    Stacks | Evaluate Expression

--- Advance/Stacks/17-November-2022.py
+++ Advance/Stacks/17-November-2022.py
@@ -60,4 +60,89 @@ class Solution:
         print("myStack = ", myStack)
         result = ''.join(myStack)
         return result
-            
+
+
+# 2 : Evaluate Expression
+
+# An arithmetic expression is given by a string array A of size N. Evaluate the value of an arithmetic expression in Reverse Polish Notation.
+
+# Valid operators are +, -, *, /. Each string may be an integer or an operator.
+
+
+# Input Format
+# The only argument given is string array A.
+
+
+# Output Format
+# Return the value of arithmetic expression formed using reverse Polish Notation.
+
+
+# Example Input
+# Input 1:
+#     A =   ["2", "1", "+", "3", "*"]
+# Input 2:
+#     A = ["4", "13", "5", "/", "+"]
+
+# Example Output
+# Output 1: 9
+# Output 2: 6
+
+
+# Example Explanation
+# Explaination 1:
+#     starting from backside:
+#     * : () * ()
+#     3 : () * (3)
+#     + : (() + ()) * (3)
+#     1 : (() + (1)) * (3)
+#     2 : ((2) + (1)) * (3)
+#     ((2) + (1)) * (3) = 9
+# Explaination 2:
+#     + : () + ()
+#     / : () + (() / ())
+#     5 : () + (() / (5))
+#     13 : () + ((13) / (5))
+#     4 : (4) + ((13) / (5))
+#     (4) + ((13) / (5)) = 6
+
+
+#Solution Approach : 
+# When you encounter an operator, that is when you need the top 2 numbers on the stack, perform the operation on them, and put them on the stack.
+
+from collections import deque
+
+class Solution:
+    def evalRPN(self, A):
+        if len(A) == 1:
+            return A[0]
+
+        myStack = deque()
+        operators = ['+', '-', '*', '/']
+
+        for i in A:
+            if i not in operators:
+                myStack.append(i)
+            else:
+                if (len(myStack) < 2):
+                    return -1
+                
+                # pop the top two elements from the stack, perform the operation 
+                # and push that back into the stack
+
+                operand1 = int(myStack.pop())
+
+                operand2 = int(myStack.pop())
+
+                operator = i 
+
+                if operator =='+':
+                    myStack.append(operand1 + operand2)
+                elif operator =='-':
+                    myStack.append(operand2 - operand1)
+                elif operator =='*':
+                    myStack.append(operand1 * operand2)
+                elif operator =='/':
+                    myStack.append(int(operand2) / (int(operand1)))
+
+        return int(myStack[0])
+
