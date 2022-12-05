f1e8c4a51afaafd20a7415028a0862c01b57bd37
rakesh050791 <rakesh050791@gmail.com>
2022-12-05 02:11:00 +0100

    Trees | Sorted Array To Balanced BST

--- Advance/Trees/02-December-2022.py
+++ Advance/Trees/02-December-2022.py
@@ -213,6 +213,18 @@ class Solution:
 
 #  There is no possible BST which have the above preorder traversal.
 
+# Solution Approach 
+
+# In Preorder traversal, descendants (or Preorder successors) of every node appear after the node.
+
+# We can say, if all internal nodes have only one child in a BST, then all the descendants of every node are either smaller or larger than the node.
+
+# To check the above condition:
+
+# Scan the last two nodes of preorder & mark them as min & max.
+
+# Scan every node down the preorder array. Each node must be either smaller than the min node or larger than the max node. Update min & max accordingly.
+
 class Solution:
     def solve(self, A):
         n = len(A)
@@ -234,3 +246,78 @@ class Solution:
         
         return 'YES'
 
+
+# 4 : Sorted Array To Balanced BST
+
+# Given an array where elements are sorted in ascending order, convert it to a height Balanced Binary Search Tree (BBST).
+
+# Balanced tree : a height-balanced binary tree is defined as a binary tree in which the depth of the two subtrees of every node never differ by more than 1.
+
+# Input Format
+# First argument is an integer array A.
+
+# Output Format
+# Return a root node of the Binary Search Tree.
+
+
+# Example Input
+# Input 1: A : [1, 2, 3]
+# Input 2: A : [1, 2, 3, 5, 10]
+
+
+# Example Output
+# Output 1:
+
+#       2
+#     /   \
+#    1     3
+# Output 2:
+
+#       3
+#     /   \
+#    2     5
+#   /       \
+#  1         10
+
+
+# Example Explanation
+# Explanation 1: You need to return the root node of the Binary Tree.
+
+
+# Solution Approach 
+
+# For a BST, all values lower than the root go in the left part of the root, and all values higher go in the right part of the root.
+# To balance the tree, we will need to make sure we distribute the elements almost equally in the left and right parts.
+# So we choose the mid part of the array as the root and divide the elements around it.
+
+# Things to take care of :
+# 1) Are you passing a copy of the array around, or are you only passing around a reference?
+# 2) Are you dividing the array before passing it onto the next function call or are you just specifying the start and end index?
+
+
+# Definition for a  binary tree node
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+class Solution:
+    # @param A : tuple of integers
+    # @return the root node in the tree
+    def sortedArrayToBST(self, A):
+        n = len(A)
+        return self.constructBalancedBST(A, 0, n-1)
+  
+    def constructBalancedBST(self, A, start, end):
+        #base condition
+        if start > end:
+            return None
+
+        mid = (start + end) // 2
+        root = TreeNode(A[mid])
+
+        root.left = self.constructBalancedBST(A, start, mid-1)
+        root.right = self.constructBalancedBST(A, mid+1, end)
+
+        return root
