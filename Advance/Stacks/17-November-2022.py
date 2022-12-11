6e8ceb4b22f6e02e9a671f3243d236bc7ca4cade
rakesh050791 <rakesh050791@gmail.com>
2022-12-11 22:44:18 +0530

    Stacks | Balanced Paranthesis

--- Advance/Stacks/17-November-2022.py
+++ Advance/Stacks/17-November-2022.py
@@ -329,3 +329,63 @@ class Solution:
 
         return myStack[-1]
 
+
+# 5 : Balanced Paranthesis
+# Given an expression string A, examine whether the pairs and the orders of “{“,”}”, ”(“,”)”, ”[“,”]” are correct in A.
+
+# Refer to the examples for more clarity.
+
+# Input Format
+# The first and the only argument of input contains the string A having the parenthesis sequence.
+
+
+
+# Output Format
+# Return 0 if the parenthesis sequence is not balanced.
+
+# Return 1 if the parenthesis sequence is balanced.
+
+
+
+# Example Input
+# Input 1: A = {([])}
+# Input 2: A = (){
+# Input 3: A = ()[] 
+
+
+# Example Output
+# Output 1: 1 
+# Output 2: 0 
+# Output 3: 1 
+
+
+# Example Explanation
+# You can clearly see that the first and third case contain valid paranthesis.
+
+# In the second case, there is no closing bracket for {, thus the paranthesis sequence is invalid.
+
+
+from collections import deque
+
+class Solution:
+    def solve(self, A):
+
+        n = len(A)
+        myStack = deque()
+
+        for i in A:
+            if (i == '{' or i == '(' or i == '['):
+                myStack.append(i)
+            else:
+                if len(myStack) == 0:
+                    return 0
+                
+                top = myStack[-1]
+
+                if ((top == '{' and i == '}') or \
+                   (top == '(' and i == ')') or \
+                   (top == '[' and i == ']')):
+                    myStack.pop()
+                
+        return 1 if len(myStack) == 0 else 0
+        
