53933a86fc3c83fe17ad39681c04b433b6a3f073
rakesh050791 <rakesh050791@gmail.com>
2022-12-15 17:48:47 +0530

    Trees | Equal Tree Partition

--- Advance/Trees/14-December-2022.py
+++ Advance/Trees/14-December-2022.py
@@ -74,3 +74,99 @@ class Solution:
         self.invertTree(A.right)
         
         return A
+
+
+# 2 : Equal Tree Partition
+
+# Given a binary tree A. Check whether it is possible to partition the tree to two trees which have equal sum of values after removing exactly one edge on the original tree.
+
+
+# Input Format
+# First and only argument is head of tree A.
+
+
+# Output Format
+# Return 1 if the tree can be partitioned into two trees of equal sum else return 0.
+
+
+
+# Example Input
+# Input 1:
+
+ 
+#                 5
+#                /  \
+#               3    7
+#              / \  / \
+#             4  6  5  6
+# Input 2:
+
+ 
+#                 1
+#                / \
+#               2   10
+#                   / \
+#                  20  2
+
+
+# Example Output
+# Output 1:
+
+#  1
+# Output 2:
+
+#  0
+
+
+# Example Explanation
+# Explanation 1:
+
+# Remove edge between 5(root node) and 7: 
+#         Tree 1 =                                               Tree 2 =
+#                         5                                                     7
+#                        /                                                     / \
+#                       3                                                     5   6    
+#                      / \
+#                     4   6
+# Sum of Tree 1 = Sum of Tree 2 = 18
+
+# Explanation 2: The given Tree cannot be partitioned.
+
+
+# Definition for a  binary tree node
+class TreeNode:
+   def __init__(self, x):
+       self.val = x
+       self.left = None
+       self.right = None
+
+import sys
+sys.setrecursionlimit(1500)
+
+class Solution:
+    # @param A : root node of tree
+    # @return an integer
+    def solve(self, A):
+        return self.checkEqualPartition(A)
+
+    def checkEqualPartition(self, A):
+        hashSet = set()
+
+        summ = self.dfs(A, hashSet)
+
+        if summ % 2:
+            return 0 
+        
+        if summ // 2 in hashSet:
+            return 1
+        return 0
+        
+    def dfs(self, root, hashSet):
+        if not root:
+            return 0 
+        
+        currentSum = root.val + self.dfs(root.left, hashSet) + self.dfs(root.right, hashSet)
+        
+        hashSet.add(currentSum)
+
+        return currentSum
