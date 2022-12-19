80067bd39a8fb7f3085b8153d580df7c7fe15652
rakesh050791 <rakesh050791@gmail.com>
2022-12-19 23:30:53 +0530

    Linked List | Reorder List

--- Advance/Linked-List/11-November-2022.py
+++ Advance/Linked-List/11-November-2022.py
@@ -166,3 +166,100 @@ class Solution:
             return s1
         else:
             return None
+
+# 3 : Reorder List 
+
+# Given a singly linked list A
+
+#  A: A0 → A1 → … → An-1 → An 
+# reorder it to:
+
+#  A0 → An → A1 → An-1 → A2 → An-2 → … 
+# You must do this in-place without altering the nodes' values.
+
+
+
+# Problem Constraints
+# 1 <= |A| <= 106
+
+
+
+# Input Format
+# The first and the only argument of input contains a pointer to the head of the linked list A.
+
+
+
+# Output Format
+# Return a pointer to the head of the modified linked list.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [1, 2, 3, 4, 5] 
+# Input 2:
+
+#  A = [1, 2, 3, 4] 
+
+
+# Example Output
+# Output 1: [1, 5, 2, 4, 3] 
+# Output 2: [1, 4, 2, 3] 
+
+
+# Example Explanation
+# Explanation 1: The array will be arranged to [A0, An, A1, An-1, A2].
+# Explanation 2: The array will be arranged to [A0, An, A1, An-1, A2].
+
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def reorderList(self, A):
+        if A.next == None or A.next.next == None:
+            return A
+
+        head = A
+
+        slow = fast = head
+        fromm = None
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+        secondLL = slow.next
+        slow.next = None
+
+        reversedSecondLL = self.reverseLL(secondLL)
+
+        current1 = head
+        current2 = reversedSecondLL
+
+        while current1 and current2:
+            next1 = current1.next
+            next2 = current2.next
+
+            current1.next = current2
+            current2.next = next1
+
+            current1 = next1
+            current2 = next2
+
+        return head
+    
+    def reverseLL(self, head):
+        temp = head
+        headOfReverseLL = None
+
+        while head != None:
+            temp = head
+            head = head.next
+            temp.next = None
+            temp.next = headOfReverseLL
+            headOfReverseLL = temp
+        
+        return headOfReverseLL
