798f55a030ff56d1ac2e25305ffdee1af89d4300
rakesh050791 <rakesh050791@gmail.com>
2022-12-22 16:39:30 +0530

    Tries | Shortest Unique Prefix

--- Advance/Tries/19-December-2022.py
+++ Advance/Tries/19-December-2022.py
@@ -90,3 +90,86 @@ class Solution:
                 currentNode = currentNode.edges[char]
 
         return currentNode.isEnd
+
+
+# 2 : Shortest Unique Prefix
+# Given a list of N words, find the shortest unique prefix to represent each word in the list.
+
+# NOTE: Assume that no word is the prefix of another. In other words, the representation is always possible
+
+
+# Input Format
+# First and only argument is a string array of size N.
+
+# Output Format
+# Return a string array B where B[i] denotes the shortest unique prefix to represent the ith word.
+
+
+# Example Input
+# Input 1: A = ["zebra", "dog", "duck", "dove"]
+# Input 2: A = ["apple", "ball", "cat"]
+
+
+# Example Output
+# Output 1: ["z", "dog", "du", "dov"]
+# Output 2: ["a", "b", "c"]
+
+
+# Example Explanation
+# Explanation 1:
+
+#  Shortest unique prefix of each word is:
+#  For word "zebra", we can only use "z" as "z" is not any prefix of any other word given.
+#  For word "dog", we have to use "dog" as "d" and "do" are prefixes of "dov".
+#  For word "du", we have to use "du" as "d" is prefix of "dov" and "dog".
+#  For word "dov", we have to use "dov" as "d" and do" are prefixes of "dog".  
+ 
+# Explanation 2: "a", "b" and c" are not prefixes of any other word. So, we can use of first letter of each to represent.
+
+class TrieNode:
+    def __init__(self, char):
+        self.char = char
+        self.edges = {}
+        self.prefixCount = 0
+
+class Solution:
+    def __init__(self):
+        self.root = TrieNode(None)
+
+    def prefix(self, A):
+        for word in A:
+            self.insert(word)
+        
+        result = []
+
+        for word in A:
+            response = self.getDistinctPF(word)
+            result.append(response)
+        
+        return result
+
+
+    def insert(self, word: str) -> None:
+        currentNode = self.root
+
+        for char in word:
+            if char not in currentNode.edges:
+                currentNode.edges[char] = TrieNode(char)
+            
+            currentNode = currentNode.edges[char]
+            currentNode.prefixCount += 1
+
+
+    def getDistinctPF(self, word: str) -> str:
+        currentNode = self.root
+        shortestPrefix = ''
+
+        for char in word:
+            shortestPrefix += char
+
+            if currentNode.edges[char].prefixCount == 1:
+                break
+            
+            currentNode = currentNode.edges[char]
+        
+        return shortestPrefix
