b2d0a2acf90e18a769b65a9bca3632c94dfd27d4
rakesh050791 <rakesh050791@gmail.com>
2022-12-28 23:03:07 +0530

    Sorting | MaxMod

--- Advance/Sorting/07-October-2022.py
+++ Advance/Sorting/07-October-2022.py
@@ -171,3 +171,72 @@ class Solution:
                 return 0
             previousElement = A[i]
         return 1
+
+# 4 : MaxMod
+# Given an array A of size N, Groot wants you to pick 2 indices i and j such that
+
+# 1 <= i, j <= N
+# A[i] % A[j] is maximum among all possible pairs of (i, j).
+# Help Groot in finding the maximum value of A[i] % A[j] for some i, j.
+
+
+# Input Format
+# First and only argument in an integer array A.
+
+
+
+# Output Format
+# Return an integer denoting the maximum value of A[i] % A[j] for any valid i, j.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [1, 2, 44, 3]
+# Input 2:
+
+#  A = [2, 6, 4]
+
+
+# Example Output
+# Output 1:
+
+#  3
+# Output 2:
+
+#  4
+
+
+# Example Explanation
+# Explanation 1:
+
+#  For i = 3, j = 2  A[i] % A[j] = 3 % 44 = 3.
+#  No pair exists which has more value than 3.
+# Explanation 2:
+
+#  For i = 2, j = 1  A[i] % A[j] = 4 % 6 = 4.
+
+# Approach: In a list of numbers, the maximum remainder is always the second largest number. (Obtained by second-largest % largest). 
+# Finding and returning this will give the answer.
+# Observation : A % B will be maximum if A < B & A, B are maximum possible (so here A will be the second larget
+# value of the array and that will be the answer)
+
+class Solution:
+    def solve(self, A):
+        n = len(A)
+        firstMax = secondMax = float('-inf')
+
+        for i in range(n):
+            if A[i] > firstMax:
+                secondMax = firstMax
+                firstMax = A[i]
+            
+            elif A[i] > secondMax and A[i] != firstMax:
+                secondMax = A[i]
+        
+        if secondMax == float('-inf'):
+            return 0
+
+        return secondMax
+        
