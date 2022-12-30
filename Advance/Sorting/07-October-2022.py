e69c20b7a037f4d36cb1ccce07f376f4e544671f
rakesh050791 <rakesh050791@gmail.com>
2022-12-30 23:48:17 +0530

    Sorting | Merge Sort

--- Advance/Sorting/07-October-2022.py
+++ Advance/Sorting/07-October-2022.py
@@ -239,4 +239,102 @@ class Solution:
             return 0
 
         return secondMax
+
+
+# 5 Merge Sort
+
+
+# Given an integer array A, sort the array using Merge Sort.
+
+
+# Problem Constraints
+# 1 <= |A| <= 105
+# 1 <= A[i] <= 109
+
+
+# Input Format
+# First argument is an integer array A.
+
+
+# Output Format
+# Return the sorted array.
+
+
+# Example Input
+# Input 1:-
+# A = [1, 4, 10, 2, 1, 5]
+# Input 2:-
+# A = [3, 7, 1]
+
+
+# Example Output
+# Output 1:-
+# [1, 1, 2, 4, 5, 10]
+# Output 2:-
+# [1, 3, 7]
+
+
+# Example Explanation
+# Explanation 1:
+# Return the sorted array.
+
+
+class Solution:
+    def solve(self, A):
+        n = len(A)
+        self.mergeSort(A, 0, n-1)
+        return A
+
+
+    def mergeSort(self, arr, start, end):
+        if start == end:
+            return
         
+        midElement = (start + end) // 2
+
+        self.mergeSort(arr, start, midElement) #Left array
+        self.mergeSort(arr, midElement + 1, end) #Right array
+        self.mergeSubarrays(arr, start, midElement, end)
+
+        return arr
+
+    def mergeSubarrays(self, arr, start, midElement, end):
+        #consider two sorted sub arrays
+        ##1 start to midElement
+        ##2 midElement+1 to end
+        p1 = start
+        p2 = midElement + 1
+        p3 = 0
+
+        tempArray = [0] * (end - start + 1)
+
+        while (p1 <= midElement and p2 <= end):
+            if arr[p1] < arr[p2]:
+                tempArray[p3] = arr[p1]
+                p1 += 1
+                p3 += 1
+            else:
+                tempArray[p3] = arr[p2]
+                p2 += 1
+                p3 += 1
+        
+        #Copying remaining elements
+        while p1 <= midElement:
+            tempArray[p3] = arr[p1]
+            p1 += 1
+            p3 += 1
+        
+        #Copying remaining elements
+        while p2 <= end:
+            tempArray[p3] = arr[p2]
+            p2 += 1
+            p3 += 1
+        
+        # copying the sorted tempArray (merged two sub arrays) to main array
+        i = start
+        j = 0 
+
+        while i <= end:
+            arr[i] = tempArray[j]
+            i += 1
+            j += 1
