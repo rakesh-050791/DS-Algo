756732db7bc426f8b4b1bab574e7c334f517d005
rakesh050791 <rakesh050791@gmail.com>
2023-01-03 11:11:40 +0530

    Heaps | Product of 3

--- Advance/Heaps/23-December-2022.py
+++ Advance/Heaps/23-December-2022.py
@@ -1,3 +1,80 @@
+# 1 : Product of 3
+
+# Given an integer array A of size N.
+
+# You have to find the product of the three largest integers in array A from range 1 to i, where i goes from 1 to N.
+
+# Return an array B where B[i] is the product of the largest 3 integers in range 1 to i in array A. If i < 3, then the integer at index i in B should be -1.
+
+# Input Format
+# First and only argument is an integer array A.
+
+# Output Format
+# Return an integer array B. B[i] denotes the product of the largest 3 integers in range 1 to i in array A.
+
+# Example Input
+# Input 1: A = [1, 2, 3, 4, 5]
+# Input 2: A = [10, 2, 13, 4]
+
+
+# Example Output
+# Output 1: [-1, -1, 6, 24, 60]
+# Output 2: [-1, -1, 260, 520]
+
+
+# Example Explanation
+# Explanation 1:
+
+#  For i = 1, ans = -1
+#  For i = 2, ans = -1
+#  For i = 3, ans = 1 * 2 * 3 = 6
+#  For i = 4, ans = 2 * 3 * 4 = 24
+#  For i = 5, ans = 3 * 4 * 5 = 60
+
+#  So, the output is [-1, -1, 6, 24, 60].
+ 
+# Explanation 2:
+
+#  For i = 1, ans = -1
+#  For i = 2, ans = -1
+#  For i = 3, ans = 10 * 2 * 13 = 260
+#  For i = 4, ans = 10 * 13 * 4 = 520
+
+#  So, the output is [-1, -1, 260, 520].
+
+
+# Approach using heap library 
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def solve(self, A):
+
+        pq = []
+        ans = []
+        for i in range(0, len(A)):
+
+            heapq.heappush(pq, -A[i])
+            # Append -1 to the answer as number of elements are < 3.
+            if i < 2:
+                ans.append(-1)
+            else:
+                # take 3 maximum elements from queue.
+                # Pop will give minimum, as we have added elements after multiplying by minus one.
+                x = heapq.heappop(pq)
+                y = heapq.heappop(pq)
+                z = heapq.heappop(pq)
+
+                val = x * y * z
+                # append -val to answer, as we have added elements after multiplying by minus one.
+                ans.append(-val)
+
+                # append all these numbers back to queue
+                heapq.heappush(pq, x)
+                heapq.heappush(pq, y)
+                heapq.heappush(pq, z)
+
+        return ans
+        
 # 2 : Maximum array sum after B negations
 
 # Given an array of integers A and an integer B. You must modify the array exactly B number of times. In a single modification, we can replace any one array element A[i] by -A[i].
