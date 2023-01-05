fadbfec335ca0783c41d8ee49bf2ed27d38ca263
rakesh050791 <rakesh050791@gmail.com>
2023-01-05 23:05:39 +0530

    Heaps | Running median

--- Advance/Heaps/04-January-2023.py
+++ Advance/Heaps/04-January-2023.py
@@ -91,3 +91,97 @@ class Solution:
             ans.append(heap.heappop(minHeap))
 
         return ans
+
+# 2 : Running median
+
+# Given an array of integers, A denoting a stream of integers. New arrays of integer B and C are formed.
+# Each time an integer is encountered in a stream, append it at the end of B and append the median of array B at the C.
+
+# Find and return the array C.
+
+# NOTE:
+
+# If the number of elements is N in B and N is odd, then consider the median as B[N/2] ( B must be in sorted order).
+# If the number of elements is N in B and N is even, then consider the median as B[N/2-1]. ( B must be in sorted order).
+
+
+# Problem Constraints
+# 1 <= length of the array <= 100000
+# 1 <= A[i] <= 109
+
+
+
+# Input Format
+# The only argument given is the integer array A.
+
+
+
+# Output Format
+# Return an integer array C, C[i] denotes the median of the first i elements.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [1, 2, 5, 4, 3]
+# Input 2:
+
+#  A = [5, 17, 100, 11]
+
+
+# Example Output
+# Output 1:
+
+#  [1, 1, 2, 2, 3]
+# Output 2:
+
+#  [5, 5, 17, 11]
+
+
+# Example Explanation
+# Explanation 1:
+
+#  stream          median
+#  [1]             1
+#  [1, 2]          1
+#  [1, 2, 5]       2
+#  [1, 2, 5, 4]    2
+#  [1, 2, 5, 4, 3] 3
+# Explanation 2:
+
+#  stream          median
+#  [5]              5
+#  [5, 17]          5
+#  [5, 17, 100]     17
+#  [5, 17, 100, 11] 11 
+
+import heapq
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def solve(self, A):
+        max_heap=[]
+        min_heap=[]
+        B=[]
+        # ans=[]
+        for i,v in enumerate(A):
+            diff=len(max_heap)-len(min_heap)
+            # print(diff)
+            if diff==0:
+                if len(min_heap)!=0 and v>min_heap[0]:
+                    min_elem=heapq.heappop(min_heap)
+                    heapq.heappush(max_heap,-min_elem)
+                    heapq.heappush(min_heap,v)
+                else:
+                    heapq.heappush(max_heap,-v)
+            else:
+                if v<-max_heap[0]:
+                    max_elem=heapq.heappop(max_heap)
+                    heapq.heappush(min_heap,-max_elem)
+                    heapq.heappush(max_heap,-v)
+                else:
+                    heapq.heappush(min_heap,v)
+            B.append(-max_heap[0])
+            # ans.append(B[i//2])
+        return B
