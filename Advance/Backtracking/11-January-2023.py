0af226bd0301b605c29162f7f865fa072c418f7c
rakesh050791 <rakesh050791@gmail.com>
2023-01-15 05:52:14 +0530

    Backtracking | Letter Phone

--- Advance/Backtracking/11-January-2023.py
+++ Advance/Backtracking/11-January-2023.py
@@ -85,7 +85,7 @@ class Solution:
 
 		currentList.pop()
 		self.generateSubsets(arr, n, currentList, indx+1)
-		
+
 
 # 2 : Subsets II
 # Given a collection of integers denoted by array A of size N that might contain duplicates, return all possible subsets.
@@ -164,8 +164,104 @@ class Solution:
         currentList.append(arr[indx])
         self.generateSubsets(arr, n, currentList, indx+1)
 
+        # to ignore duplicate we can compare next element and move pointer to next unique indx 
         while indx < n-1 and arr[indx] == arr[indx+1]:
             indx += 1
 
         currentList.pop()
         self.generateSubsets(arr, n, currentList, indx+1)
+
+
+# 3 : Letter Phone
+
+# Given a digit string A, return all possible letter combinations that the number could represent.
+
+# A mapping of digit to letters (just like on the telephone buttons) is given below.
+
+
+
+# The digit 0 maps to 0 itself. The digit 1 maps to 1 itself.
+
+# NOTE: Make sure the returned strings are lexicographically sorted.
+
+
+
+# Problem Constraints
+# 1 <= |A| <= 10
+
+
+
+# Input Format
+# The only argument is a digit string A.
+
+
+
+# Output Format
+# Return a string array denoting the possible letter combinations.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = "23"
+# Input 2:
+
+#  A = "012"
+
+
+# Example Output
+# Output 1:
+
+#  ["ad", "ae", "af", "bd", "be", "bf", "cd", "ce", "cf"]
+# Output 2:
+
+#  ["01a", "01b", "01c"]
+
+
+# Example Explanation
+# Explanation 1:
+
+#  There are 9 possible letter combinations.
+# Explanation 2:
+
+#  Only 3 possible letter combinations.
+
+class Solution:
+	# @param A : string
+	# @return a list of strings
+	def letterCombinations(self, A):
+        if not A:
+            return []
+        phonedict = {
+            '0': "0",
+            '1': "1",
+            '2': "abc",
+            '3': "def",
+            '4': "ghi",
+            '5': "jkl",
+            '6': "mno",
+            '7': "pqrs",
+            '8': 'tuv',
+            '9': "wxyz",
+        }
+        
+        N = len(A)
+        res = []
+        def generateA(index,cur_string):
+            if len(cur_string) == N:
+                res.append(cur_string)
+                return 
+            
+            number = A[index]
+            cur_char = phonedict[number]
+            
+            for char in cur_char:
+                # add char and call the next digit
+                generateA(index + 1,cur_string + char)
+            
+            
+             
+        generateA(0,"")
+        return res 
+
