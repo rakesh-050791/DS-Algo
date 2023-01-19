8943a9855b7efa5f5ae0a4d254fb8fa96e113bea
rakesh050791 <rakesh050791@gmail.com>
2023-01-19 12:40:38 +0530

    Binary Search | Special Integer

--- Advance/Binary-Search/17-October-2022.py
+++ Advance/Binary-Search/17-October-2022.py
@@ -124,3 +124,98 @@ class Solution:
             elif mid % 2 != 0:
                 # search on left
                 high = mid - 1
+
+# 3 : Special Integer
+
+# Given an array of integers A and an integer B, find and return the maximum value K such that there is no subarray in A of size K with the sum of elements greater than B.
+
+
+
+# Problem Constraints
+# 1 <= |A| <= 100000
+# 1 <= A[i] <= 10^9
+
+# 1 <= B <= 10^9
+
+
+
+# Input Format
+# The first argument given is the integer array A.
+
+# The second argument given is integer B.
+
+
+
+# Output Format
+# Return the maximum value of K (sub array length).
+
+
+
+# Example Input
+# Input 1:
+
+# A = [1, 2, 3, 4, 5]
+# B = 10
+# Input 2:
+
+# A = [5, 17, 100, 11]
+# B = 130
+
+
+# Example Output
+# Output 1:
+
+#  2
+# Output 2:
+
+#  3
+
+
+# Example Explanation
+# Explanation 1:
+
+# Constraints are satisfied for maximal value of 2.
+# Explanation 2:
+
+# Constraints are satisfied for maximal value of 3.
+
+class Solution:
+    def solve(self, A, B):
+        n = len(A)
+
+        low = 0
+        high = n
+        result = 0
+
+        while (low <= high):
+            mid = (low + high) // 2 
+
+            if self.slidingWindow(A, mid, B):
+                result = mid
+                low = mid + 1
+            else:
+                high = mid - 1
+        
+        return result
+    
+    def slidingWindow(self, arr, windowSize, B):
+        windowSum = 0
+        n = len(arr)
+
+        #calculating first window sum
+        for i in range(windowSize):
+            windowSum += arr[i]
+            if windowSum > B:
+                return False
+        
+        #now slide window & calculate sum 
+        ## below both J loops are working, need to figure out which one to keep
+        
+        for j in range(n - windowSize):
+            windowSum -= arr[j]
+            windowSum += arr[j + windowSize]
+
+            if windowSum > B:
+                return False 
+
+        return True
