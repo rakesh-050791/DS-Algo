70d199b8db21ab0f0ace0873d985446fd1e09703
rakesh050791 <rakesh050791@gmail.com>
2023-01-21 11:56:25 +0530

    Binary Search | Matrix Search

--- Advance/Binary-Search/13-October-2022.py
+++ Advance/Binary-Search/13-October-2022.py
@@ -268,3 +268,112 @@ class Solution:
                 low = mid + 1
         
         return lastOccurence
+
+
+# 5 : Matrix Search
+
+# Given a matrix of integers A of size N x M and an integer B. Write an efficient algorithm that searches for integer B in matrix A.
+
+# This matrix A has the following properties:
+
+# Integers in each row are sorted from left to right.
+# The first integer of each row is greater than or equal to the last integer of the previous row.
+# Return 1 if B is present in A, else return 0.
+
+# NOTE: Rows are numbered from top to bottom, and columns are from left to right.
+
+
+
+# Problem Constraints
+# 1 <= N, M <= 1000
+# 1 <= A[i][j], B <= 106
+
+
+
+# Input Format
+# The first argument given is the integer matrix A.
+# The second argument given is the integer B.
+
+
+
+# Output Format
+# Return 1 if B is present in A else, return 0.
+
+
+
+# Example Input
+# Input 1:
+
+# A = [ 
+#       [1,   3,  5,  7]
+#       [10, 11, 16, 20]
+#       [23, 30, 34, 50]  
+#     ]
+# B = 3
+# Input 2:
+
+# A = [   
+#       [5, 17, 100, 111]
+#       [119, 120, 127, 131]    
+#     ]
+# B = 3
+
+
+# Example Output
+# Output 1:
+
+# 1
+# Output 2:
+
+# 0
+
+
+# Example Explanation
+# Explanation 1:
+
+#  3 is present in the matrix at A[0][1] position so return 1.
+# Explanation 2:
+
+#  3 is not present in the matrix so return 0.
+
+class Solution:
+    # @param A : list of list of integers
+    # @param B : integer
+    # @return an integer
+    def searchMatrix(self, A, B):
+        rows = len(A)
+        cols = len(A[0])
+
+        # finding the potential row
+        l = 0
+        h = rows - 1
+        # potential_row = 0
+        while(l <= h):
+            mid = (l + h) // 2
+
+            if A[mid][0] == B:
+                return 1
+            elif A[mid][0] < B:
+                l = mid + 1
+            else:
+                h = mid - 1
+       
+        row = l - 1
+
+        # Now that we have the row, apply BS to find B in that row
+        # print(row)
+        l = 0
+        h = cols - 1
+
+        while(l <= h):
+            mid = (l + h) // 2
+
+            if A[row][mid] == B:
+                return 1
+            elif A[row][mid] < B:
+                l = mid + 1
+            else:
+                h = mid - 1
+       
+        return 0
+        
