9722e98ea1f9dc428110dc0b94b77cb810359070
rakesh050791 <rakesh050791@gmail.com>
2023-01-22 22:27:41 +0530

    Two-Pointer | Sum Zero

--- Advance/Two-Pointer/28-October-2022.py
+++ Advance/Two-Pointer/28-October-2022.py
@@ -277,4 +277,83 @@ class Solution:
         
         return maxArea
 
+# 5 : Sum Zero
+# Given an array A of N integers, are there elements a, b, c in S such that a + b + c = 0
 
+# Find all unique triplets in the array which gives the sum of zero.
+
+# Note:
+
+# Elements in a triplet (a,b,c) must be in non-descending order. (ie, a ≤ b ≤ c) The solution set must not contain duplicate triplets.
+
+
+
+# Problem Constraints
+
+# 0 <= N <= 7000
+
+# -108 <= A[i] <= 108
+
+
+
+# Input Format
+
+# Single argument representing a 1-D array A.
+
+
+
+# Output Format
+
+# Output a 2-D vector where each row represent a unique triplet.
+
+
+
+# Example Input
+
+# A = [-1,0,1,2,-1,4]
+
+
+# Example Output
+
+# [ [-1,0,1],
+#   [-1,-1,2] ]
+
+
+# Example Explanation
+
+# Out of all the possible triplets having total sum zero,only the above two triplets are unique.
+
+class Solution:
+    # @param A : list of integers
+    # @return a list of list of integers
+    def threeSum(self, A):
+        A = sorted(A) 
+        ans = []
+        
+        for i in range(len(A)-2):
+            # to avoid duplicate triplet
+            if i > 0 and A[i] == A[i-1]:
+                continue
+            
+            j = i + 1
+            k = len(A) - 1
+            target = -(A[i]) 
+            while j < k:
+                cur_sum =  A[j] + A[k]
+                if cur_sum == target:
+                    triplet = [-target,A[j],A[k]]
+                    ans.append([-target,A[j],A[k]])
+                    # another conditional for not calculating duplicates
+                    while j < k and A[j] == A[j+1]:
+                        j += 1
+                        
+                    while j < k and A[k] == A[k-1]:
+                        k -= 1
+
+                    j = j + 1
+                    k = k - 1                    
+                if cur_sum < target:
+                    j = j + 1
+                elif cur_sum > target:
+                    k = k - 1
+        return ans
