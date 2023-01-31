37e8a955dbb23d07f4f6d8fb7612dd8f3e1b68d4
rakesh050791 <rakesh050791@gmail.com>
2023-01-31 20:13:30 +0100

    Two Pointers | Sort by Color | DUTCH NATIONAL FLAG ALGO

--- Advance/Two-Pointer/28-October-2022.py
+++ Advance/Two-Pointer/28-October-2022.py
@@ -563,3 +563,99 @@ class Solution:
         return val
 
 
+# 8 : Sort by Color | DUTCH NATIONAL FLAG ALGO 
+
+# Given an array with N objects colored red, white, or blue, sort them so that objects of the same color are adjacent, with the colors in the order red, white, and blue.
+
+# We will use the integers 0, 1, and 2 to represent red, white, and blue, respectively.
+
+# Note: Using the library sort function is not allowed.
+
+
+
+# Problem Constraints
+# 1 <= N <= 1000000
+# 0 <= A[i] <= 2
+
+
+# Input Format
+# First and only argument of input contains an integer array A.
+
+
+# Output Format
+# Return an integer array in asked order
+
+
+# Example Input
+# Input 1 :
+#     A = [0 1 2 0 1 2]
+# Input 2:
+
+#     A = [0]
+
+
+# Example Output
+# Output 1:
+#     [0 0 1 1 2 2]
+# Output 2:
+
+#     [0]
+
+
+# Example Explanation
+# Explanation 1:
+#     [0 0 1 1 2 2] is the required order.
+
+
+
+# Approach
+
+# 1)sort the array with custom comparator function.
+
+# 2)use cmp_to_key to compare & change the original array list.sort()
+# 3)while creating the custom comparator function return 1,0,-1 for your result as mentioned
+
+# optimized 
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def sortColors(self, A):
+            l = 0
+            m = 0
+            h = len(A) - 1
+
+            while m <= h:
+                if A[m] == 1:
+                    m += 1
+                
+                elif A[m] == 0:
+                    temp = A[m]
+                    A[m] = A[l]
+                    A[l] = temp
+                    m += 1
+                    l += 1
+                
+                elif A[m] == 2:
+                    temp1 = A[m]
+                    A[m] = A[h]
+                    A[h] = temp1
+                    h -= 1
+            return A
+
+
+#BRUTEFORCE:
+   def sortColors(self, A):
+        count_0 = []
+        count_1 = []
+        count_2 = []
+
+        for i in A:
+            if i == 0:
+                count_0.append(i)
+            elif i == 1:
+                count_1.append(i)
+            else:
+                count_2.append(i)
+               
+        return(count_0+count_1+count_2)
+
