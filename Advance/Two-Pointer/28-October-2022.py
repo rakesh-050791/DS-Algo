b494f5a9ec9c431dff06c5ac35b4bc0ba79b0cd3
rakesh050791 <rakesh050791@gmail.com>
2023-02-01 23:30:11 +0100

    Two Pointers | Closest pair from sorted arrays

--- Advance/Two-Pointer/28-October-2022.py
+++ Advance/Two-Pointer/28-October-2022.py
@@ -659,3 +659,119 @@ class Solution:
                
         return(count_0+count_1+count_2)
 
+
+# 9 : Closest pair from sorted arrays
+
+# Given two sorted arrays of distinct integers, A and B, and an integer C, find and return the pair whose sum is closest to C and the pair has one element from each array.
+
+# More formally, find A[i] and B[j] such that abs((A[i] + B[j]) - C) has minimum value.
+
+# If there are multiple solutions find the one with minimum i and even if there are multiple values of j for the same i then return the one with minimum j.
+
+# Return an array with two elements {A[i], B[j]}.
+
+
+
+# Problem Constraints
+
+# 1 <= length of both the arrays <= 100000
+
+# 1 <= A[i], B[i] <= 109
+
+# 1 <= C <= 109
+
+
+
+# Input Format
+
+# The first argument given is the integer array A.
+# The second argument given is the integer array B.
+# The third argument given is integer C.
+
+
+
+# Output Format
+
+# Return an array of size 2 denoting the pair which has sum closest to C.
+
+
+
+# Example Input
+
+# Input 1:
+
+#  A = [1, 2, 3, 4, 5]
+#  B = [2, 4, 6, 8]
+#  C = 9
+# Input 2:
+
+#  A = [5, 10, 20]
+#  B = [1, 2, 30]
+#  C = 13
+
+
+# Example Output
+
+# Output 1:
+
+#  [1, 8]
+# Output 2:
+
+#  [10, 2]
+
+
+# Example Explanation
+
+# Explanation 1:
+
+#  There are three pairs: (1, 8), (3, 6), (5, 4), that gives the minimum value.
+#  Since we have to return the value with minimum i and then with minimum j. We will return [1, 8].
+# Explanation 2:
+
+#  [10, 2] is the only pair such abs(10+2-13) is minimum.
+
+class Solution:
+    # @param A : list of integers
+    # @param B : list of integers
+    # @param C : integer
+    # @return a list of integers
+    def solve(self, A, B, C):
+        last_sum = float('inf') 
+        low = 0
+        high = len(B) - 1
+
+        first_indx = 0
+        secnd_indx = -1
+
+        while low <= len(A)-1 and high >= 0:
+            
+            cur_sum = A[low] + B[high]
+            
+            if abs(cur_sum - C) < last_sum:
+                # min-diff check with last answer
+                last_sum = abs(cur_sum - C)
+                first_indx = low
+                secnd_indx = high
+
+
+            elif abs(cur_sum - C) == last_sum:
+                # if sum is equal then we need min - i index answer
+
+                if low < first_indx:
+                    first_indx = low
+                    secnd_indx = high
+
+                # if i is same , then min j index check 
+                elif low == first_indx and high < secnd_indx:
+                    first_indx = low
+                    secnd_indx = high
+
+
+
+            if cur_sum > C:
+                high -= 1
+            else:
+                low += 1
+                
+        return(A[first_indx],B[secnd_indx])
+
