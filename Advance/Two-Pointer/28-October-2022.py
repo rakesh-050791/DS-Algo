77f16d9aee75a87f321327960a7433069c055458
rakesh050791 <rakesh050791@gmail.com>
2023-02-03 21:12:50 +0100

    Two Pointers | Another Count Rectangles

--- Advance/Two-Pointer/28-October-2022.py
+++ Advance/Two-Pointer/28-October-2022.py
@@ -864,3 +864,87 @@ class Solution:
 
 
         return ans
+
+# 11 : Another Count Rectangles
+
+# Given a sorted array of distinct integers A and an integer B, find and return how many rectangles with distinct configurations can be created using elements of this array as length and breadth whose area is lesser than B.
+
+# (Note that a rectangle of 2 x 3 is different from 3 x 2 if we take configuration into view)
+
+
+
+# Problem Constraints
+
+# 1 <= |A| <= 100000
+# 1 <= A[i] <= 109
+# 1 <= B <= 109
+
+
+
+# Input Format
+
+# The first argument given is the integer array A.
+
+# The second argument given is integer B.
+
+
+
+# Output Format
+
+# Return the number of rectangles with distinct configurations with area less than B modulo (109 + 7).
+
+
+
+# Example Input
+
+# Input 1:
+
+#  A = [1, 2]
+#  B = 5
+# Input 2:
+
+#  A = [1, 2]
+#  B = 1
+
+
+# Example Output
+
+# Output 1:
+
+#  4
+# Output 2:
+
+#  0
+
+
+# Example Explanation
+
+# Explanation 1:
+
+#  All 1X1, 2X2, 1X2 and 2X1 have area less than 5.
+# Explanation 2:
+
+#  No Rectangle is valid.
+
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+        A.sort()
+        lenA= len(A)
+        start,end = 0,lenA-1
+        modulo = 1000000007
+        count = 0
+
+        while end>=0 and start<lenA:
+            area = A[start] * A[end]
+         
+            if area<B :
+                count +=(end+1)
+                start +=1
+            else:
+                end -=1
+            count %= modulo
+
+        return count%modulo
