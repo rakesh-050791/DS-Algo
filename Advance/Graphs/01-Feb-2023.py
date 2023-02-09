c6c08d3bb0106a972445e46d8f57909c3063dee4
rakesh050791 <rakesh050791@gmail.com>
2023-02-09 23:25:23 +0100

    Graphs | Shortest Distance in a Maze

--- Advance/Graphs/01-Feb-2023.py
+++ Advance/Graphs/01-Feb-2023.py
@@ -215,3 +215,114 @@ class Solution:
 
         return 0
 
+# 3 : Shortest Distance in a Maze
+# Given a matrix of integers A of size N x M describing a maze. The maze consists of empty locations and walls.
+
+# 1 represents a wall in a matrix and 0 represents an empty location in a wall.
+
+# There is a ball trapped in a maze. The ball can go through empty spaces by rolling up, down, left or right, but it won't stop rolling until hitting a wall (maze boundary is also considered as a wall). When the ball stops, it could choose the next direction.
+
+# Given two array of integers of size B and C of size 2 denoting the starting and destination position of the ball.
+
+# Find the shortest distance for the ball to stop at the destination. The distance is defined by the number of empty spaces traveled by the ball from the starting position (excluded) to the destination (included). If the ball cannot stop at the destination, return -1.
+
+
+
+# Problem Constraints
+# 2 <= N, M <= 100
+
+# 0 <= A[i] <= 1
+
+# 0 <= B[i][0], C[i][0] < N
+
+# 0 <= B[i][1], C[i][1] < M
+
+
+
+# Input Format
+# The first argument given is the integer matrix A.
+
+# The second argument given is an array of integer B.
+
+# The third argument if an array of integer C.
+
+
+
+# Output Format
+# Return a single integer, the minimum distance required to reach destination
+
+
+
+# Example Input
+# Input 1:
+
+# A = [ [0, 0], [0, 0] ]
+# B = [0, 0]
+# C = [0, 1]
+# Input 2:
+
+# A = [ [0, 0], [0, 1] ]
+# B = [0, 0]
+# C = [0, 1]
+
+
+# Example Output
+# Output 1:
+
+#  1
+# Output 2:
+
+#  1
+
+
+# Example Explanation
+# Explanation 1:
+
+#  Go directly from start to destination in distance 1.
+# Explanation 2:
+
+#  Go directly from start to destination in distance 1.
+
+
+import heapq as hq 
+class Solution:
+    # @param A : list of list of integers
+    # @param B : list of integers
+    # @param C : list of integers
+    # @return an integer
+    def solve(self, grid, source, destination):
+        N , M = len(grid) , len(grid[0])
+        
+        min_heap = []
+        hq.heappush(min_heap,(0,source[0],source[1]))                   # (Distance,Src,Node)
+        visited        = [[False] * M for _ in range(N)]
+        
+        directions     = [[0,1],[1,0],[0,-1],[-1,0]]
+
+        while min_heap:
+            distance , cr , cc    =  hq.heappop(min_heap)
+
+            if cr == destination[0] and cc == destination[1]:
+                return distance
+
+            if visited[cr][cc]: 
+                continue
+
+            visited[cr][cc] = True 
+
+            org_cr  = cr
+            org_cc  = cc
+            org_dis = distance
+            for i , j in directions:
+                cr =  org_cr
+                cc =  org_cc
+                cd =  org_dis
+                # move cur_point to all dirtns untl hits wall
+                while cr + i >= 0 and cr + i < N and cc + j >= 0 and cc + j < M and grid[cr+i][cc+j] == 0: 
+                    cr += i          
+                    cc += j
+                    cd += 1 
+                
+                hq.heappush(min_heap,(cd,cr,cc))
+                
+        return -1
