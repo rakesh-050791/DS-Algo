df72aaba3d100b7ebcaaa6eda852e6fad806f907
rakesh050791 <rakesh050791@gmail.com>
2023-02-11 22:03:20 +0100

    Graphs | Possibility of Finishing

--- Advance/Graphs/06-Feb-2023.py
+++ Advance/Graphs/06-Feb-2023.py
@@ -108,3 +108,110 @@ class Solution:
                 if ind[u]==0:
                     pq.put(u)
         return ans
+
+
+# 2 : Possibility of Finishing
+
+# There are a total of A courses you have to take, labeled from 1 to A.
+
+# Some courses may have prerequisites, for example to take course 2 you have to first take course 1, which is expressed as a pair: [1,2].
+
+# So you are given two integer array B and C of same size where for each i (B[i], C[i]) denotes a pair.
+
+# Given the total number of courses and a list of prerequisite pairs, is it possible for you to finish all courses?
+
+# Return 1 if it is possible to finish all the courses, or 0 if it is not possible to finish all the courses.
+
+
+
+# Problem Constraints
+# 1 <= A <= 6*104
+
+# 1 <= length(B) = length(C) <= 105
+
+# 1 <= B[i], C[i] <= A
+
+
+
+# Input Format
+# The first argument of input contains an integer A, representing the number of courses.
+
+# The second argument of input contains an integer array, B.
+
+# The third argument of input contains an integer array, C.
+
+
+
+# Output Format
+# Return 1 if it is possible to finish all the courses, or 0 if it is not possible to finish all the courses.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = 3
+#  B = [1, 2]
+#  C = [2, 3]
+# Input 2:
+
+#  A = 2
+#  B = [1, 2]
+#  C = [2, 1]
+
+
+# Example Output
+# Output 1:
+
+#  1
+# Output 2:
+
+#  0
+
+
+# Example Explanation
+# Explanation 1:
+
+#  It is possible to complete the courses in the following order:
+#     1 -> 2 -> 3
+# Explanation 2:
+
+#  It is not possible to complete all the courses.
+
+class Solution:
+	# @param A : integer
+	# @param B : list of integers
+	# @param C : list of integers
+	# @return an integer
+	def solve(self, A, B, C):
+        from queue import  PriorityQueue 
+        #create min heap/priority queue
+        pq=PriorityQueue()
+        
+        g=[[] for i in range(A+1)]
+        ans=[]
+        ind=[0 for i in range(A+1)]
+        #generate adjacency list of DAG
+        for i in range(len(B)):
+            u=B[i]
+            v=C[i]
+            g[u].append(v)
+            ind[v]+=1
+        
+        for i in range(1,A+1):
+                    if ind[i]==0:
+                        pq.put(i)
+        c=0        
+        while not (pq.empty()) > 0:
+            #get the top element from priority queue and insert to ans
+            front=pq.get()
+            c+=1
+            #for each of the front node, do a BFS
+            for i in range(len(g[front])):
+                u=g[front][i]
+                ind[u]-=1
+                if ind[u]==0:
+                    pq.put(u)
+        if c!=A:
+            return 0
+        return 1
