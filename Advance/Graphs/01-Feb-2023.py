82282910fa7ab2a8fc27d750b681a7fd3f371588
rakesh050791 <rakesh050791@gmail.com>
2023-02-15 21:27:46 +0100

    Graphs | Number of islands

--- Advance/Graphs/01-Feb-2023.py
+++ Advance/Graphs/01-Feb-2023.py
@@ -326,3 +326,122 @@ class Solution:
                 hq.heappush(min_heap,(cd,cr,cc))
                 
         return -1
+ 
+# 4 : Number of islands
+# Given a matrix of integers A of size N x M consisting of 0 and 1. A group of connected 1's forms an island. From a cell (i, j) such that A[i][j] = 1 you can visit any cell that shares a corner with (i, j) and value in that cell is 1.
+
+# More formally, from any cell (i, j) if A[i][j] = 1 you can visit:
+
+# (i-1, j) if (i-1, j) is inside the matrix and A[i-1][j] = 1.
+# (i, j-1) if (i, j-1) is inside the matrix and A[i][j-1] = 1.
+# (i+1, j) if (i+1, j) is inside the matrix and A[i+1][j] = 1.
+# (i, j+1) if (i, j+1) is inside the matrix and A[i][j+1] = 1.
+# (i-1, j-1) if (i-1, j-1) is inside the matrix and A[i-1][j-1] = 1.
+# (i+1, j+1) if (i+1, j+1) is inside the matrix and A[i+1][j+1] = 1.
+# (i-1, j+1) if (i-1, j+1) is inside the matrix and A[i-1][j+1] = 1.
+# (i+1, j-1) if (i+1, j-1) is inside the matrix and A[i+1][j-1] = 1.
+# Return the number of islands.
+
+# NOTE: Rows are numbered from top to bottom and columns are numbered from left to right.
+
+
+
+# Problem Constraints
+# 1 <= N, M <= 100
+
+# 0 <= A[i] <= 1
+
+
+
+# Input Format
+# The only argument given is the integer matrix A.
+
+
+
+# Output Format
+# Return the number of islands.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [ 
+#        [0, 1, 0]
+#        [0, 0, 1]
+#        [1, 0, 0]
+#      ]
+# Input 2:
+
+#  A = [   
+#        [1, 1, 0, 0, 0]
+#        [0, 1, 0, 0, 0]
+#        [1, 0, 0, 1, 1]
+#        [0, 0, 0, 0, 0]
+#        [1, 0, 1, 0, 1]    
+#      ]
+
+
+# Example Output
+# Output 1:
+
+#  2
+# Output 2:
+
+#  5
+
+
+# Example Explanation
+# Explanation 1:
+
+#  The 1's at position A[0][1] and A[1][2] forms one island.
+#  Other is formed by A[2][0].
+# Explanation 2:
+
+#  There 5 island in total.
+
+import sys 
+sys.setrecursionlimit(10**6)
+
+class Solution:
+    def backtracking(self,i,j,mat,N,M):
+        if i < 0 or i >= N or j < 0 or j >= M:
+            return 
+        
+        if mat[i][j] == 0:
+            return 
+        
+        if mat[i][j] == 1:
+            mat[i][j] = 0              # make it visited 
+            
+            # go top 
+            self.backtracking(i-1,j,mat,N,M)
+            # go left
+            self.backtracking(i,j-1,mat,N,M)
+            # go down
+            self.backtracking(i+1,j,mat,N,M)
+            # go right
+            self.backtracking(i,j+1,mat,N,M)
+
+
+
+            self.backtracking(i-1, j-1,mat,N,M)
+            self.backtracking(i+1, j+1,mat,N,M)
+            self.backtracking(i-1, j+1,mat,N,M)
+            self.backtracking(i+1, j-1,mat,N,M)
+
+
+        
+    
+    def solve(self,grid):
+        N , M = len(grid) , len(grid[0])
+        
+        islands = 0
+        for i in range(N):
+            for j in range(M):
+                if grid[i][j] == 1:
+                    self.backtracking(i,j,grid,N,M)
+                    islands += 1
+        return islands
+
+ 
