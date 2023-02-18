562e288527f188db814f8facab7d56bf41276f04
rakesh050791 <rakesh050791@gmail.com>
2023-02-18 20:37:37 +0100

    Sorting | Unique Elements

--- Advance/Sorting/10-October-2022.py
+++ Advance/Sorting/10-October-2022.py
@@ -326,3 +326,67 @@ class Solution:
         
         return result
         
+# 5 : Unique Elements
+
+# You are given an array A of N elements. You have to make all elements unique. To do so, in one step you can increase any number by one.
+
+# Find the minimum number of steps.
+
+
+
+# Problem Constraints
+# 1 <= N <= 105
+# 1 <= A[i] <= 109
+
+
+
+# Input Format
+# The only argument given is an Array A, having N integers.
+
+
+
+# Output Format
+# Return the minimum number of steps required to make all elements unique.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [1, 1, 3]
+# Input 2:
+
+#  A = [2, 4, 5]
+
+
+# Example Output
+# Output 1:
+
+#  1
+# Output 2:
+
+#  0
+
+
+# Example Explanation
+# Explanation 1:
+
+#  We can increase the value of 1st element by 1 in 1 step and will get all unique elements. i.e [2, 1, 3].
+# Explanation 2:
+
+#  All elements are distinct.
+
+class Solution:
+    def solve(self, A):
+        n = len(A)
+
+        A.sort()
+        minimumSteps = 0 
+
+        for i in range(1, n):
+            if A[i] <= A[i-1]:
+                minimumSteps += ((A[i - 1] + 1) - A[i])
+                A[i] +=  ((A[i - 1] + 1) - A[i]) 
+        return minimumSteps
+
+        
