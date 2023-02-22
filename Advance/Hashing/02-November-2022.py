87cca63de8f294d90091c69b4917b5917fe4316a
rakesh050791 <rakesh050791@gmail.com>
2023-02-22 20:59:13 +0100

    Hashing | Common Elements

--- Advance/Hashing/02-November-2022.py
+++ Advance/Hashing/02-November-2022.py
@@ -144,3 +144,84 @@ class Solution:
             element = (str(x), str(y))
             hashSet.add(element)
         return len(hashSet)
+
+
+# 3 : Common Elements
+
+# Given two integer arrays, A and B of size N and M, respectively. Your task is to find all the common elements in both the array.
+
+# NOTE:
+
+# Each element in the result should appear as many times as it appears in both arrays.
+# The result can be in any order.
+
+
+# Problem Constraints
+# 1 <= N, M <= 105
+
+# 1 <= A[i] <= 109
+
+
+
+# Input Format
+# First argument is an integer array A of size N.
+
+# Second argument is an integer array B of size M.
+
+
+
+# Output Format
+# Return an integer array denoting the common elements.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [1, 2, 2, 1]
+#  B = [2, 3, 1, 2]
+# Input 2:
+
+#  A = [2, 1, 4, 10]
+#  B = [3, 6, 2, 10, 10]
+
+
+# Example Output
+# Output 1:
+
+#  [1, 2, 2]
+# Output 2:
+
+#  [2, 10]
+
+
+# Example Explanation
+# Explanation 1:
+
+#  Elements (1, 2, 2) appears in both the array. Note 2 appears twice in both the array.
+# Explantion 2:
+
+#  Elements (2, 10) appears in both the array.
+
+class Solution:
+    def solve(self, A, B):
+        n = len(A)
+        m = len(B)
+
+        hashMapA = {}
+        result = []
+
+        for k in A:
+            if k in hashMapA:
+                hashMapA[k] += 1
+            else:
+                hashMapA[k] = 1
+
+        for i in range(m):
+            if B[i] in hashMapA:
+                if hashMapA[B[i]] > 0:
+                    result.append(B[i])
+                    hashMapA[B[i]] -= 1
+        
+        return result
+        
