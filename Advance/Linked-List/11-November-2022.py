b231a313d38005c581c3b9d2a408586ccb188715
rakesh050791 <rakesh050791@gmail.com>
2023-02-23 21:24:24 +0100

    Linked List | Intersection of Linked Lists

--- Advance/Linked-List/11-November-2022.py
+++ Advance/Linked-List/11-November-2022.py
@@ -263,3 +263,116 @@ class Solution:
             headOfReverseLL = temp
         
         return headOfReverseLL
+
+# 4 : Intersection of Linked Lists
+# Write a program to find the node at which the intersection of two singly linked lists, A and B, begins. For example, the following two linked lists:
+
+# A:          a1 → a2
+#                    ↘
+#                      c1 → c2 → c3
+#                    ↗
+# B:     b1 → b2 → b3
+# NOTE:
+
+# If the two linked lists have no intersection at all, return null.
+# The linked lists must retain their original structure after the function returns.
+# You may assume there are no cycles anywhere in the entire linked structure.
+# Your code should preferably run in O(n) time and use only O(1) memory.
+# The custom input to be given is different than the one explained in the examples. Please be careful.
+
+
+# Problem Constraints
+
+# 0 <= |A|, |B| <= 106
+
+
+
+# Input Format
+
+# The first argument of input contains a pointer to the head of the linked list A.
+
+# The second argument of input contains a pointer to the head of the linked list B.
+
+
+
+# Output Format
+
+# Return a pointer to the node after which the linked list is intersecting.
+
+
+
+# Example Input
+
+# Input 1:
+
+#  A = [1, 2, 3, 4, 5]
+#  B = [6, 3, 4, 5]
+# Input 2:
+
+#  A = [1, 2, 3]
+#  B = [4, 5]
+
+
+# Example Output
+
+# Output 1:
+
+#  [3, 4, 5]
+# Output 2:
+
+#  []
+
+
+# Example Explanation
+
+# Explanation 1:
+
+#  In the first example, the nodes have the same values after 3rd node in A and 2nd node in B. Thus, the linked lists are intersecting after that point. 
+# Explanation 2:
+
+#  In the second example, the nodes don't have the same values, thus we can return None/Null. 
+
+
+'''
+Definition for singly-linked list
+class ListNode:
+   def __init__(self, val):
+      self.val = val
+      self.next = None
+'''
+class Solution:
+    def getIntersectionNode(self, A, B):
+        list1 = A
+        list2 = B
+
+        list1_len = 0
+        list2_len = 0
+
+        cur = list1
+        while cur:
+            list1_len += 1
+            cur = cur.next
+
+        cur = list2
+        while cur:
+            list2_len += 1
+            cur = cur.next
+
+        bigger_list, smaller_list = (A, B) if list1_len > list2_len else (B, A)
+
+        len_diff = abs(list1_len-list2_len)
+
+        # Advance bigger list by difference 
+        while len_diff > 0:
+            len_diff -= 1
+            bigger_list = bigger_list.next
+
+        # Both the lists are at same level when advanced and checked, should meet
+        # if they are connected
+        while bigger_list and smaller_list:
+            if bigger_list == smaller_list:
+                return bigger_list
+            bigger_list = bigger_list.next
+            smaller_list = smaller_list.next
+
+        return None
