8733761937dc35c0888d91092df2d061e7222b2a
rakesh050791 <rakesh050791@gmail.com>
2023-02-26 19:57:37 +0100

    Maths | PrimeNumber | Distinct Primes

--- Advance/Maths/PrimeNumbers/28-September-2022.py
+++ Advance/Maths/PrimeNumbers/28-September-2022.py
@@ -359,3 +359,74 @@ class Solution:
 
         return int(A**0.5)
 
+# 7 : Distinct Primes
+# You have given an array A having N integers. Let say G is the product of all elements of A.
+
+# You have to find the number of distinct prime divisors of G.
+
+
+
+# Input Format
+
+# The first argument given is an Array A, having N integers.
+# Output Format
+
+# Return an Integer, i.e number of distinct prime divisors of G.
+# Constraints
+
+# 1 <= N <= 1e5
+# 1 <= A[i] <= 1e5
+# For Example
+
+# Input:
+#     A = [1, 2, 3, 4]
+# Output:
+#      2
+
+# Explanation:
+#     here G = 1 * 2 * 3 * 4 = 24
+#     and distinct prime divisors of G are [2, 3]
+
+class Solution:
+# @param A : list of integers
+# @return an integer
+   def solve(self, A):
+       # spf reference sieve for prime factorisation 
+       ceil = max(A)
+       spf = [0] * (ceil + 1)
+
+
+       # marking the sieve
+       for i in range(ceil + 1):
+           spf[i] = i
+
+
+       # prime filter
+       stepper = 2
+       while stepper * stepper <= ceil:
+           if spf[stepper] == stepper:
+               # prime number
+               expunge = stepper * stepper
+               while expunge <= ceil:
+                   if spf[expunge] == expunge:
+                       spf[expunge] = stepper
+                   expunge += stepper
+           stepper += 1
+
+       # to track distinct prime factors
+       distinct_prime_divisors = set()
+
+       for a in A:
+           while a > 1:
+               div = spf[a]
+               distinct_prime_divisors.add(div)
+               while a % div == 0:
+                    a //= div
+       return len(distinct_prime_divisors)
+
+# ‘’‘
+# M -> max element in the array
+# N -> array size
+# TC: O(NlogM + loglogM) => O(NlogM)
+# SC: O(M)
+# ‘’’
