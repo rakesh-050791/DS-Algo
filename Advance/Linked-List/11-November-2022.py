d1c073b1261cf4dbc7b48738579f12598ddcaa8b
rakesh050791 <rakesh050791@gmail.com>
2023-02-27 21:46:35 +0100

    Linked List | Merge Two Sorted Lists

--- Advance/Linked-List/11-November-2022.py
+++ Advance/Linked-List/11-November-2022.py
@@ -376,3 +376,102 @@ class Solution:
             smaller_list = smaller_list.next
 
         return None
+
+# 5 : Merge Two Sorted Lists
+
+# Merge two sorted linked lists, A and B, and return it as a new list.
+
+# The new list should be made by splicing together the nodes of the first two lists and should also be sorted.
+
+
+
+# Problem Constraints
+# 0 <= |A|, |B| <= 105
+
+
+
+# Input Format
+# The first argument of input contains a pointer to the head of linked list A.
+
+# The second argument of input contains a pointer to the head of linked list B.
+
+
+
+# Output Format
+# Return a pointer to the head of the merged linked list.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = 5 -> 8 -> 20
+#  B = 4 -> 11 -> 15
+# Input 2:
+
+#  A = 1 -> 2 -> 3
+#  B = Null
+
+
+# Example Output
+# Output 1:
+
+#  4 -> 5 -> 8 -> 11 -> 15 -> 20
+# Output 2:
+
+#  1 -> 2 -> 3
+
+
+# Example Explanation
+# Explanation 1:
+
+#  Merging A and B will result in 4 -> 5 -> 8 -> 11 -> 15 -> 20 
+# Explanation 2:
+
+#  We don't need to merge as B is empty. 
+
+# Definition for singly-linked list.
+# class ListNode:
+#   def __init__(self, x):
+#       self.val = x
+#       self.next = None
+
+class Solution:
+    # @param A : head node of linked list
+    # @param B : head node of linked list
+    # @return the head node in the linked list
+    def mergeTwoLists(self, list1, list2):
+        if not list1: return list2
+        if not list2: return list1
+        if not list1 and list2: return None
+        
+        # vote head
+        head = None
+        if list1.val < list2.val: 
+            head  = list1 
+            list1 = list1.next
+        else:
+            head  = list2
+            list2 = list2.next
+            
+        # start merging 
+        tail = head
+        while list1 and list2:
+            if list1.val < list2.val:
+                tail.next = list1
+                list1     = list1.next
+            else:
+                tail.next = list2
+                list2     = list2.next
+                
+            tail = tail.next
+            
+            
+        # attach left over
+        if list1: 
+            tail.next = list1
+        else: 
+            tail.next = list2
+            
+            
+        return head
