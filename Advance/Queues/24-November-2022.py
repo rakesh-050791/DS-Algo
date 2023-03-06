4a1dcb4d4fa1f21147fdf7cb7f16d52eac11d5a1
rakesh050791 <rakesh050791@gmail.com>
2023-03-06 22:15:23 +0100

    Queues | Reversing Elements Of Queue

--- Advance/Queues/24-November-2022.py
+++ Advance/Queues/24-November-2022.py
@@ -212,3 +212,79 @@ class Solution:
 
             A -= 1
         return output
+
+# 4 : Reversing Elements Of Queue
+
+# Given an array of integers A and an integer B, we need to reverse the order of the first B elements of the array, leaving the other elements in the same relative order.
+
+# NOTE: You are required to the first insert elements into an auxiliary queue then perform Reversal of first B elements.
+
+
+
+# Problem Constraints
+# 1 <= B <= length of the array <= 500000
+# 1 <= A[i] <= 100000
+
+
+
+# Input Format
+# The argument given is the integer array A and an integer B.
+
+
+
+# Output Format
+# Return an array of integer after reversing the first B elements of A using queue.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [1, 2, 3, 4, 5]
+#  B = 3
+# Input 2:
+
+#  A = [5, 17, 100, 11]
+#  B = 2
+
+
+# Example Output
+# Output 1:
+
+#  [3, 2, 1, 4, 5]
+# Output 2:
+
+#  [17, 5, 100, 11]
+
+
+# Example Explanation
+# Explanation 1:
+
+#  Reverse first 3 elements so the array becomes [3, 2, 1, 4, 5]
+# Explanation 2:
+
+#  Reverse first 2 elements so the array becomes [17, 5, 100, 11]
+
+
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return a list of integers
+    def solve(self, A, B):
+            que=[]
+            for i in reversed(range(B)):  
+                que.append(A[i])
+            ans=len(A)-B
+            if ans>0:
+                s=B        # starting index
+                e=len(A)   # end index
+                while s<e:
+                    que.append(A[s])
+                    s+=1
+            return que
+    # T.C: O(N) & S.C: O(N)  
+    # code Explanation: for loop in range reversed of B--> range=1,0
+    # so in que A[1] & A[0] will append
+    # for dry run visualization : click on -->    shorturl.at/dpv34
+
+
