2c0fd4ba447e20b807d31c9d2ba42e619e1b083a
rakesh050791 <rakesh050791@gmail.com>
2023-03-07 22:25:31 +0100

    Maths | GCD | All GCD Pair

--- Advance/Maths/GCD/26-September-2022.py
+++ Advance/Maths/GCD/26-September-2022.py
@@ -180,3 +180,93 @@ class Solution:
 
         return self.gcd(b, a % b)
     
+
+# 5 : All GCD Pair
+
+# Given an array of integers A of size N containing GCD of every possible pair of elements of another array.
+
+# Find and return the original numbers used to calculate the GCD array in any order. For example, if original numbers are {2, 8, 10} then the given array will be {2, 2, 2, 2, 8, 2, 2, 2, 10}.
+
+
+
+# Problem Constraints
+# 1 <= N <= 10000
+
+# 1 <= A[i] <= 109
+
+
+
+# Input Format
+# The first and only argument given is the integer array A.
+
+
+
+# Output Format
+# Find and return the original numbers which are used to calculate the GCD array in any order.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [2, 2, 2, 2, 8, 2, 2, 2, 10]
+# Input 2:
+
+#  A = [5, 5, 5, 15]
+
+
+# Example Output
+# Output 1:
+
+#  [2, 8, 10]
+# Output 2:
+
+#  [5, 15]
+
+
+# Example Explanation
+# Explanation 1:
+
+#  Initially, array A = [2, 2, 2, 2, 8, 2, 2, 2, 10].
+#  2 is the gcd between 2 and 8, 2 and 10.
+#  8 and 10 are the gcds pair with itself.
+#  Therefore, [2, 8, 10] is the original array.
+# Explanation 2:
+
+#  Initially, array A = [5, 5, 5, 15].
+#  5 is the gcd between 5 and 15.
+#  15 is the gcds pair with itself.
+#  Therefore, [5, 15] is the original array.
+
+def GCD(x,y):
+    if y==0:
+        return abs(x)
+    return GCD(y,x%y)
+
+
+class Solution:
+    # @param A : list of integers
+    # @return a list of integers
+    def solve(self, A):
+        N=len(A)
+        A.sort()
+        for i in range(N//2):# sorted in decreasing order for finding highest elements
+            A[i],A[N-1-i]=A[N-1-i],A[i]
+
+        ans=[]
+        di={} # di stores the count of A[i]'s that are to be deleted from the array
+        for i in range(N):
+            if A[i] in di and di[A[i]]>0:
+                di[A[i]]-=1
+           
+            else:
+                for j in ans:
+                    val=GCD(A[i],j)# first highest elemnts will get into ans array
+                    if val not in di:
+                        di[val]=2
+                        # we are adding 2 to the di as there will 2 pairs gcd(ans[j],A[i]) and gcd(A[i],ans[j])
+                    else:
+                        di[val]+=2    
+                ans.append(A[i])
+
+        return ans 
