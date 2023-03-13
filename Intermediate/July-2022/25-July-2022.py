c9d2e574d7d78460c604b178f0545681bcff5497
rakesh050791 <rakesh050791@gmail.com>
2023-03-13 21:14:07 +0100

    Bit Manipulation | Unset x bits from right

--- Intermediate/July-2022/25-July-2022.py
+++ Intermediate/July-2022/25-July-2022.py
@@ -111,3 +111,66 @@ class Solution:
             A = A >> 1
         return numberOfSetBits
 
+
+# 3 :  Unset x bits from right
+
+# Given an integer A. Unset B bits from the right of A in binary.
+# For eg:-
+# A = 93, B = 4
+# A in binary = 1011101
+# A should become = 1010000 = 80. Therefore return 80.
+
+
+# Problem Constraints
+# 1 <= A <= 1018
+# 1 <= B <= 60
+
+
+# Input Format
+# The first argument is a single integer A.
+# The second argument is a single integer B.
+
+
+# Output Format
+# Return the number with B unset bits from the right.
+
+
+# Example Input
+# Input 1:-
+# A = 25
+# B = 3
+# Input 2:-
+# A = 37
+# B = 3
+
+
+# Example Output
+# Output 1:-
+# 24
+# Output 2:-
+# 32
+
+
+# Example Explanation
+# Explanation 1:-
+# A = 11001 to 11000
+# Explantio 2:-
+# A = 100101 to 100000
+
+
+
+# class Solution:
+#     # @param A : long
+#     # @param B : integer
+#      # @return an long
+#     def solve(self, A, B):
+
+#         A = A>>B; #// Unsets any set numbers by Bth place, for example: 1011>>0010
+#         A = A<<B;# // Brings back digits post Bth place, our answer, for example 1000<<0010
+#         return A;
+class Solution:
+    # @param A : long
+    # @param B : integer
+    # @return an long
+    def solve(self, A, B):
+        return (A >> B) << B
