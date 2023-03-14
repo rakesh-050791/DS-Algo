308a1f7b0ed23a36222fff92dd97cf2ceea90623
rakesh050791 <rakesh050791@gmail.com>
2023-03-14 21:12:07 +0100

    Bit Manipulation | Subarrays with Bitwise OR 1

--- Intermediate/July-2022/25-July-2022.py
+++ Intermediate/July-2022/25-July-2022.py
@@ -174,3 +174,65 @@ class Solution:
     # @return an long
     def solve(self, A, B):
         return (A >> B) << B
+
+
+# 4 : Subarrays with Bitwise OR 1
+# Given an array B of length A with elements 1 or 0. Find the number of subarrays such that the bitwise OR of all the elements present in the subarray is 1.
+
+
+# Problem Constraints
+# 1 <= A <= 105
+
+
+# Input Format
+# The first argument is a single integer A.
+# The second argument is an integer array B.
+
+
+# Output Format
+# Return the number of subarrays with bitwise array 1.
+
+
+# Example Input
+# Input 1:
+#  A = 3
+# B = [1, 0, 1]
+# Input 2:
+#  A = 2
+# B = [1, 0]
+
+
+# Example Output
+# Output 1:
+# 5
+# Output2:
+# 2
+
+
+# Example Explanation
+# Explanation 1:
+# The subarrays are :- [1], [0], [1], [1, 0], [0, 1], [1, 0, 1]
+# Except the subarray [0] all the other subarrays has a Bitwise OR = 1
+# Explanation 2:
+# The subarrays are :- [1], [0], [1, 0]
+# Except the subarray [0] all the other subarrays has a Bitwise OR = 1
+
+class Solution:
+    # @param A : integer
+    # @param B : list of integers
+     # @return an long
+    def solve(self, A, B):
+        ans = 0
+        prev_subarray_count = 0
+
+        for i in range(A):
+            if B[i] == 1:
+                current_subarray_count = i+1 # when ith element(rightMostElement) = 1. no. of subarrays= i+1
+                ans += current_subarray_count
+                prev_subarray_count = current_subarray_count
+
+            else:
+                ans += prev_subarray_count # when ith element = 0. then no of subarrays = previousSubarrayCount
+
+        return(ans)
+
