5a8fd2b96aa9309ff07b4a5acca685486fe86c77
rakesh050791 <rakesh050791@gmail.com>
2023-03-17 00:01:21 +0100

    Arrays | Amazing Subarrays

--- Intermediate/August-2022/05-Aug-2022.py
+++ Intermediate/August-2022/05-Aug-2022.py
@@ -188,3 +188,51 @@ class Solution:
         return 0 if nonAlphanumericCount > 0 else 1
 
 
+# 8 : Amazing Subarrays
+
+# You are given a string S, and you have to find all the amazing substrings of S.
+
+# An amazing Substring is one that starts with a vowel (a, e, i, o, u, A, E, I, O, U).
+
+# Input
+
+# Only argument given is string S.
+# Output
+
+# Return a single integer X mod 10003, here X is the number of Amazing Substrings in given the string.
+# Constraints
+
+# 1 <= length(S) <= 1e6
+# S can have special characters
+# Example
+
+# Input
+#     ABEC
+
+# Output
+#     6
+
+# Explanation
+#     Amazing substrings of given string are :
+#     1. A
+#     2. AB
+#     3. ABE
+#     4. ABEC
+#     5. E
+#     6. EC
+#     here number of substrings are 6 and 6 % 10003 = 6.
+
+    class Solution:
+    # @param A : string
+    # @return an integer
+    def solve(self, A):
+            N = len(A)
+            no_elements = 0
+            count = 0
+            vowels = ('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U')
+            for i in range(N-1, -1, -1):
+                no_elements += 1
+                if A[i] in vowels:
+                    count += no_elements
+            return count%10003
+
