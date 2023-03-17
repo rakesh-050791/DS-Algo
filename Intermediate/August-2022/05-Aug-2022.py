e50a8fcaf6d02b1e1b5d08ff88795a7177d08a87
rakesh050791 <rakesh050791@gmail.com>
2023-03-17 23:51:53 +0100

    Arrays | Count Occurrences

--- Intermediate/August-2022/05-Aug-2022.py
+++ Intermediate/August-2022/05-Aug-2022.py
@@ -236,3 +236,58 @@ class Solution:
                     count += no_elements
             return count%10003
 
+# 9 : Count Occurrences
+
+# Find the number of occurrences of bob in string A consisting of lowercase English alphabets.
+
+
+
+# Problem Constraints
+# 1 <= |A| <= 1000
+
+
+# Input Format
+# The first and only argument contains the string A, consisting of lowercase English alphabets.
+
+
+# Output Format
+# Return an integer containing the answer.
+
+
+# Example Input
+# Input 1:
+
+#   "abobc"
+# Input 2:
+
+#   "bobob"
+
+
+# Example Output
+# Output 1:
+
+#   1
+# Output 2:
+
+#   2
+
+
+# Example Explanation
+# Explanation 1:
+
+#   The only occurrence is at second position.
+# Explanation 2:
+
+#   Bob occures at first and third position.
+
+class Solution:
+    # @param A : string
+    # @return an integer
+    def solve(self, A):
+        n=len(A)
+        ans="bob"
+        coun=0
+        for i in range(n-2):
+            if A[i:i+3]==ans:
+                coun+=1
+        return coun
