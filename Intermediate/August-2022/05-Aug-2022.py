57f102be0d0c9cbf1008b731da6427d4f6d0ab07
rakesh050791 <rakesh050791@gmail.com>
2023-03-25 21:35:03 +0100

    Strings | String operations

--- Intermediate/August-2022/05-Aug-2022.py
+++ Intermediate/August-2022/05-Aug-2022.py
@@ -361,3 +361,68 @@ class Solution:
                 count-=1    # those alphabets whose freq=0 will be dedcuted until we get total no of elemnts in our string
                 B-=index[i]  
         return count    
+
+
+# 11 : String operations
+
+# Akash likes playing with strings. One day he thought of applying following operations on the string in the given order:
+
+# Concatenate the string with itself.
+# Delete all the uppercase letters.
+# Replace each vowel with '#'.
+# You are given a string A of size N consisting of lowercase and uppercase alphabets. Return the resultant string after applying the above operations.
+
+# NOTE: 'a' , 'e' , 'i' , 'o' , 'u' are defined as vowels.
+
+
+
+# Problem Constraints
+
+# 1<=N<=100000
+
+
+# Input Format
+
+# First argument is a string A of size N.
+
+
+
+# Output Format
+
+# Return the resultant string.
+
+
+
+# Example Input
+
+# A="AbcaZeoB"
+
+
+
+# Example Output
+
+# "bc###bc###"
+
+
+
+# Example Explanation
+
+# First concatenate the string with itself so string A becomes "AbcaZeoBAbcaZeoB".
+# Delete all the uppercase letters so string A becomes "bcaeobcaeo".
+# Now replace vowel with '#'.
+# A becomes "bc###bc###".
+
+class Solution:
+    # @param A : string
+    # @return a strings
+    def solve(self, A):
+        ans = ""
+        for i in range(len(A)):
+            if A[i].isupper():
+                ans+= ""
+            elif A[i] in 'aeiou':
+                ans+= '#'
+            else:
+                ans += A[i]    
+
+        return ans+ans
