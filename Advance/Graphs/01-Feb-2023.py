893aba5ac77f98847ee49b0e15fbd69ad1258915
rakesh050791 <rakesh050791@gmail.com>
2023-03-28 21:54:04 +0200

    Graphs | Valid Path

--- Advance/Graphs/01-Feb-2023.py
+++ Advance/Graphs/01-Feb-2023.py
@@ -85,7 +85,7 @@ class Solution:
             val = B[i][1]
             adjList[indx].append(val)
             # OR : One liner below
-            
+
             # adjList[B[indx][0]].append(B[indx][1])
 
             #OR another way to create adjacency list
@@ -660,5 +660,126 @@ class Solution:
 
 
 
+# 7 : Valid Path
+# There is a rectangle with left bottom as (0, 0) and right up as (x, y).
+
+# There are N circles such that their centers are inside the rectangle.
+
+# Radius of each circle is R. Now we need to find out if it is possible that we can move from (0, 0) to (x, y) without touching any circle.
+
+# Note : We can move from any cell to any of its 8 adjecent neighbours and we cannot move outside the boundary of the rectangle at any point of time.
 
 
+
+# Problem Constraints
+# 0 <= x , y, R <= 100
+
+# 1 <= N <= 1000
+
+# Center of each circle would lie within the grid
+
+
+
+# Input Format
+# 1st argument given is an Integer x , denoted by A in input.
+
+# 2nd argument given is an Integer y, denoted by B in input.
+
+# 3rd argument given is an Integer N, number of circles, denoted by C in input.
+
+# 4th argument given is an Integer R, radius of each circle, denoted by D in input.
+
+# 5th argument given is an Array A of size N, denoted by E in input, where A[i] = x cordinate of ith circle
+
+# 6th argument given is an Array B of size N, denoted by F in input, where B[i] = y cordinate of ith circle
+
+
+
+# Output Format
+# Return YES or NO depending on weather it is possible to reach cell (x,y) or not starting from (0,0).
+
+
+
+# Example Input
+# Input 1:
+
+#  x = 2
+#  y = 3
+#  N = 1
+#  R = 1
+#  A = [2]
+#  B = [3]
+# Input 2:
+
+#  x = 1
+#  y = 1
+#  N = 1
+#  R = 1
+#  A = [1]
+#  B = [1]
+
+
+# Example Output
+# Output 1:
+
+#  NO
+# Output 2:
+
+#  NO
+
+
+# Example Explanation
+# Explanation 1:
+
+#  There is NO valid path in this case
+# Explanation 2:
+
+#  There is NO valid path in this case
+
+
+class Solution:
+    def dfs(self,i,j,mat,N,M):
+        if i < 0 or j < 0 or i > N or j > M:
+            return False
+
+        if i == N and j == M:
+            return True
+
+        if mat[i][j] == 1:
+            mat[i][j] = 0
+
+            directions = [ [1,0],[0,1],[-1,0],[0,-1],[-1,-1],[1,1],[-1,1],[1,-1] ]
+
+            for rd , cd in directions:
+                cur_row = rd + i
+                cur_col = cd + j
+
+                if self.dfs(cur_row,cur_col,mat,N,M):
+                    return True
+
+        return False
+       
+
+
+    def solve(self, x, y, Circles, Radius, Xarr, Yarr):
+        matrix = [[1]* (y+1) for _ in range(x+1)]
+
+        for i in range(x+1):
+            for j in range(y+1):
+                for k in range(Circles):
+                    dist = pow(i - Xarr[k], 2) + pow( j - Yarr[k], 2);
+                   
+                    if(dist <= Radius*Radius):
+                        matrix[i][j] = 0
+
+       
+        if matrix[0][0] == 0 or matrix[x][y] == 0:
+            return "NO"
+
+       
+        if not self.dfs(0,0,matrix,x,y):
+            return "NO"
+
+        return "YES"
+               
+        
