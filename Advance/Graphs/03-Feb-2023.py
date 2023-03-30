da1bb8ebe88984183c94225dc3fa3824d0712af8
rakesh050791 <rakesh050791@gmail.com>
2023-03-30 21:30:51 +0200

    Graphs | Rotten Oranges

--- Advance/Graphs/03-Feb-2023.py
+++ Advance/Graphs/03-Feb-2023.py
@@ -57,3 +57,101 @@ class Solution:
             return 2
         else:
             return 3
+
+
+# 2 : Rotten Oranges
+# Given a matrix of integers A of size N x M consisting of 0, 1 or 2.
+
+# Each cell can have three values:
+
+# The value 0 representing an empty cell.
+
+# The value 1 representing a fresh orange.
+
+# The value 2 representing a rotten orange.
+
+# Every minute, any fresh orange that is adjacent (Left, Right, Top, or Bottom) to a rotten orange becomes rotten. Return the minimum number of minutes that must elapse until no cell has a fresh orange. If this is impossible, return -1 instead.
+
+# Note: Your solution will run on multiple test cases. If you are using global variables, make sure to clear them.
+
+# Problem Constraints
+# 1 <= N, M <= 1000
+
+# 0 <= A[i][j] <= 2
+
+# Input Format
+# The first argument given is the integer matrix A.
+
+# Output Format
+# Return the minimum number of minutes that must elapse until no cell has a fresh orange.
+
+# If this is impossible, return -1 instead.
+
+# Example Input
+# Input 1:
+
+# A = [   [2, 1, 1]
+#         [1, 1, 0]
+#         [0, 1, 1]   ]
+# Input 2:
+
+ 
+# A = [   [2, 1, 1]
+#         [0, 1, 1]
+#         [1, 0, 1]   ]
+
+
+# Example Output
+# Output 1:   4
+# Output 2:  -1
+
+
+# Example Explanation
+# Explanation 1: Max of 4 using (0,0) , (0,1) , (1,1) , (1,2) , (2,2)
+
+# Explanation 2: Task is impossible
+
+from collections import deque
+
+class Solution:
+    # @param A : list of list of integers
+    # @return an integer
+    def solve(self, A):
+        rows = len(A)
+        cols = len(A[0])
+        grid = A
+
+        time , freshOranges = 0, 0
+
+        q = deque()
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == 1:
+                    freshOranges += 1
+
+                if grid[r][c] ==2:
+                    q.append([r,c])
+        
+        directions = [ [0, 1], [0, -1], [1, 0], [-1, 0] ]
+
+        while q and freshOranges > 0:
+            for i in range(len(q)):
+                r , c = q.popleft()
+
+                for dr, dc in directions:
+                    row, col = dr + r, dc + c
+
+                    if (row < 0 or row == len(grid) or 
+                        col < 0 or col == len(grid[0]) or 
+                        grid[row][col] != 1):
+
+                        continue
+
+                    grid[row][col] = 2
+                    q.append([row, col])
+                    freshOranges -= 1
+            
+            time += 1
+        
+        return time if freshOranges == 0 else -1
