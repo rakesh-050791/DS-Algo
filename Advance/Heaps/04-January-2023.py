0729cab3c4c988ab9e32c28ea83aed23f9f64b0e
rakesh050791 <rakesh050791@gmail.com>
2023-04-09 21:04:35 +0200

    Heaps | Ways to form Max Heap

--- Advance/Heaps/04-January-2023.py
+++ Advance/Heaps/04-January-2023.py
@@ -273,3 +273,92 @@ class Solution:
             ans.append(temp[0])
         return ans
 
+
+# 4 : Ways to form Max Heap
+
+# Max Heap is a special kind of complete binary tree in which, for every node, the value present in that node is greater than the value present in its children nodes.
+
+# Find the number of distinct Max Heap that can be made from A distinct integers.
+
+# In short, you have to ensure the following properties for the max heap :
+
+# Heap has to be a complete binary tree ( A complete binary tree is a binary tree in which every level, except possibly the last, is completely filled, and all nodes are as far left as possible.)
+# Every node is greater than all its children.
+# NOTE: If you want to know more about Heaps, please visit this link. Return your answer modulo 109 + 7.
+
+
+
+# Problem Constraints
+# 1 <= A <= 100
+
+
+
+# Input Format
+# The first and only argument is an integer A.
+
+
+
+# Output Format
+# Return an integer denoting the number of distinct Max Heap.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = 4
+# Input 2:
+
+#  A = 10
+
+
+# Example Output
+# Output 1:
+
+#  3
+# Output 2:
+
+#  3360
+
+
+# Example Explanation
+# Explanation 1:
+
+#  Let us take 1, 2, 3, 4 as our 4 distinct integers
+#  Following are the 3 possible max heaps from these 4 numbers :
+#       4           4                     4
+#     /  \         / \                   / \ 
+#    3    2   ,   2   3      and        3   1
+#   /            /                     /    
+#  1            1                     2
+# Explanation 2:
+
+#  Number of distinct heaps possible with 10 distinct integers = 3360.
+
+MOD = 10**9+7
+
+class Solution:
+    # @param A : integer
+    # @return an integer
+    
+    def comb(self,r,n) :
+        if 2*r > n : 
+            return self.comb(n-r,n)
+        c = 1
+        for i in range(r) :
+            c = c*(n-i)//(i+1)
+        return c
+    
+    def solve(self, A):
+        ans,h = [1,1], 0
+        for n in range(2,A+1) :
+            if 2<<h <= n :
+                h += 1
+            m = n-(1<<h)+1
+            l = (1<<(h-1))-1 + min(m,1<<(h-1))
+            r = (1<<(h-1))-1 + max(0,m-(1<<(h-1)))
+            ans.append((self.comb(l,n-1)*ans[l]*ans[r])%MOD)
+        return ans[A]
+
+
+
