99aae10fc9b762f1e2d648ee26a52278e5d5b929
rakesh050791 <rakesh050791@gmail.com>
2023-04-12 02:09:50 +0200

    Dynamic Programming | Minimum Number of Squares | Improved top down approach

--- Advance/Dynamic-Programming/16-January-2023.py
+++ Advance/Dynamic-Programming/16-January-2023.py
@@ -136,24 +136,28 @@ import sys
 sys.setrecursionlimit(10**6)
 
 class Solution:
-    # @param A : integer
-    # @return an integer
-    def helper(self, A, dp):
-        if dp[A] != -1: return dp[A]
-
-        ans = A
-        i = 1
-        while i * i <= A:
-            ans = min(ans, 1 + self.helper(A - i * i, dp))
-            i += 1
-
-        dp[A] = ans
-        return ans
-
     def countMinSquares(self, A):
-        dp = [-1] * (A + 1)
+        dp = [-1] * (A+1)
         dp[0], dp[1] = 0, 1
-        return self.helper(A, dp)
+        return self.psquares(A, dp)
+
+    def psquares(self, n, dp):
+        if n == 0:
+            return 0
+
+        if dp[n] != -1:
+            return dp[n]
+
+        ans = n
+        x = 1
+
+        while (x * x <= n):
+            ans = min(ans, self.psquares(n - x * x, dp) )
+            x += 1
+        
+        dp[n] = 1 + ans
+
+        return dp[n]
 
     # TC: O(NsqrtN); SC: O(N)
 
@@ -175,3 +179,4 @@ class Solution:
         return dp[A]
 
         # TC: O(NsqrtN); SC: O(N)
+
