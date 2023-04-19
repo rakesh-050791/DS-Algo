7299a1f792a55a851ee9c5de9c61a8ac2f885fdd
rakesh050791 <rakesh050791@gmail.com>
2023-04-19 23:44:17 +0200

    Dynamic Programming | N digit numbers

--- Advance/Dynamic-Programming/18-January-2023.py
+++ Advance/Dynamic-Programming/18-January-2023.py
@@ -88,3 +88,99 @@ class Solution:
 
         return dp[i][j]
 
+
+# 2 : N digit numbers
+
+# Find out the number of A digit positive numbers, whose digits on being added equals to a given number B.
+
+# Note that a valid number starts from digits 1-9 except the number 0 itself. i.e. leading zeroes are not allowed.
+
+# Since the answer can be large, output answer modulo 1000000007
+
+
+
+# Problem Constraints
+# 1 <= A <= 1000
+
+# 1 <= B <= 10000
+
+
+
+# Input Format
+# First argument is the integer A
+
+# Second argument is the integer B
+
+
+
+# Output Format
+# Return a single integer, the answer to the problem
+
+
+
+# Example Input
+# Input 1:
+
+#  A = 2
+#  B = 4
+# Input 2:
+
+#  A = 1
+#  B = 3
+
+
+# Example Output
+# Output 1:
+
+#  4
+# Output 2:
+
+#  1
+
+
+# Example Explanation
+# Explanation 1:
+
+#  Valid numbers are {22, 31, 13, 40}
+#  Hence output 4.
+# Explanation 2:
+
+#  Only valid number is 3
+
+MODULO = 1000000007
+class Solution:
+    # @param A : integer
+    # @param B : integer
+    # @return an integer
+    def solve(self, A, B):
+
+        dp = [[-1]*(B+1) for _ in range(A+1)]
+        dp[0][0] = 1
+
+        output = 0
+
+        for i in range(1, 10):
+            if (B - i >= 0):
+                output += self.digitsCount(dp, A-1, B-i)
+                output %= MODULO
+        
+        return output
+
+    def digitsCount(self, dp, A, summ):
+        if (A == 0):
+            return summ == 0
+
+        if dp[A][summ] != -1:
+            return dp[A][summ]
+
+        result = 0
+
+        for i in range(10):
+            if (summ - i >= 0):
+                result += self.digitsCount(dp, A-1, summ-i)
+                result %= MODULO 
+        dp[A][summ] = result
+        
+        return dp[A][summ]
+
+
