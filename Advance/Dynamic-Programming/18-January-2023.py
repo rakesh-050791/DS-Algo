15bd61f127cec05c20f8e9447dd7e2e9d820bd34
rakesh050791 <rakesh050791@gmail.com>
2023-04-20 20:49:02 +0200

    Dynamic Programming | Dungeon Princess

--- Advance/Dynamic-Programming/18-January-2023.py
+++ Advance/Dynamic-Programming/18-January-2023.py
@@ -159,6 +159,8 @@ class Solution:
 
         output = 0
 
+        # Traverse through every digit from 1 to
+        # 9 and count numbers beginning with it
         for i in range(1, 10):
             if (B - i >= 0):
                 output += self.digitsCount(dp, A-1, B-i)
@@ -170,11 +172,17 @@ class Solution:
         if (A == 0):
             return summ == 0
 
+        # If this subproblem is already evaluated,
+        # return the evaluated value
+
         if dp[A][summ] != -1:
             return dp[A][summ]
 
         result = 0
 
+        # Traverse through every digit and
+        # recursively count numbers beginning
+        # with it
         for i in range(10):
             if (summ - i >= 0):
                 result += self.digitsCount(dp, A-1, summ-i)
@@ -182,5 +190,110 @@ class Solution:
         dp[A][summ] = result
         
         return dp[A][summ]
+
+
+# 3 : Dungeon Princess
+
+# The demons had captured the princess and imprisoned her in the bottom-right corner of a dungeon. The dungeon consists of M x N rooms laid out in a 2D grid. Our valiant knight was initially positioned in the top-left room and must fight his way through the dungeon to rescue the princess.
+
+# The knight has an initial health point represented by a positive integer. If at any point his health point drops to 0 or below, he dies immediately.
+
+# Some of the rooms are guarded by demons, so the knight loses health (negative integers) upon entering these rooms; other rooms are either empty (0's) or contain magic orbs that increase the knight's health (positive integers).
+
+# In order to reach the princess as quickly as possible, the knight decides to move only rightward or downward in each step.
+
+# Given a 2D array of integers A of size M x N. Find and return the knight's minimum initial health so that he is able to rescue the princess.
+
+
+
+# Problem Constraints
+# 1 <= M, N <= 500
+
+# -100 <= A[i] <= 100
+
+
+
+# Input Format
+# First and only argument is a 2D integer array A denoting the grid of size M x N.
+
+
+
+# Output Format
+# Return an integer denoting the knight's minimum initial health so that he is able to rescue the princess.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [ 
+#        [-2, -3, 3],
+#        [-5, -10, 1],
+#        [10, 30, -5]
+#      ]
+# Input 2:
+
+#  A = [ 
+#        [1, -1, 0],
+#        [-1, 1, -1],
+#        [1, 0, -1]
+#      ]
+
+
+# Example Output
+# Output 1:
+
+#  7
+# Output 2:
+
+#  1
+
+
+# Example Explanation
+# Explanation 1:
+
+#  Initially knight is at A[0][0].
+#  If he takes the path RIGHT -> RIGHT -> DOWN -> DOWN, the minimum health required will be 7.
+#  At (0,0) he looses 2 health, so health becomes 5.
+#  At (0,1) he looses 3 health, so health becomes 2.
+#  At (0,2) he gains 3 health, so health becomes 5.
+#  At (1,2) he gains 1 health, so health becomes 6.
+#  At (2,2) he looses 5 health, so health becomes 1.
+#  At any point, the health point doesn't drop to 0 or below. So he can rescue the princess with minimum health 7.
+ 
+# Explanation 2:
+
+#  Take the path DOWN -> DOWN ->RIGHT -> RIGHT, the minimum health required will be 1.
+
+import sys 
+sys.setrecursionlimit(10**6)
+
+class Solution:
+    # @param A : list of list of integers
+    # @return an integer
+    def calculateMinimumHP(self, A):
+        n = len(A)
+        m = len(A[0])
+
+        dp = [[-1] * m for _ in range(n)]
+
+        return self.minHealth(A, dp, 0, 0)
+
+    def minHealth(self, mat, dp, i, j):
+        if (i == len(mat)-1 and j == len(mat[0])-1):
+            return max(1, 1 - mat[i][j])
+
+        if (i == len(mat) or j == len(mat[0])):
+            return float('inf')
+
+        if dp[i][j] != -1:
+            return dp[i][j]
+
+        dp[i][j] = max(min(self.minHealth(mat, dp, i+1, j), self.minHealth(mat, dp, i, j+1)) - mat[i][j], 1)
+
+        return dp[i][j]
+
+
+
 
 
