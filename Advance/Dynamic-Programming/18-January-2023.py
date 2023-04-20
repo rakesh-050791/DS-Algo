f0914e56162630f03a9a84ffbe2689999f6d4858
rakesh050791 <rakesh050791@gmail.com>
2023-04-21 00:07:08 +0200

    Dynamic Programming | Min Sum Path in Triangle

--- Advance/Dynamic-Programming/18-January-2023.py
+++ Advance/Dynamic-Programming/18-January-2023.py
@@ -295,5 +295,87 @@ class Solution:
 
 
 
+# 4 : Min Sum Path in Triangle
+
+# Given a triangle, find the minimum path sum from top to bottom. Each step you may move to adjacent numbers on the row below.
+
+# Adjacent numbers for jth column of ith row is jth and (j+1)th column of (i + 1)th row
+
+
+
+# Problem Constraints
+# |A| <= 1000
+
+# A[i] <= 1000
+
+
+
+# Input Format
+# First and only argument is the vector of vector A defining the given triangle
+
+
+
+# Output Format
+# Return the minimum sum
+
+
+
+# Example Input
+# Input 1:
+
+ 
+# A = [ 
+#          [2],
+#         [3, 4],
+#        [6, 5, 7],
+#       [4, 1, 8, 3]
+#     ]
+# Input 2:
+
+#  A = [ [1] ]
+
+
+# Example Output
+# Output 1:
+
+#  11
+# Output 2:
+
+#  1
+
+
+# Example Explanation
+# Explanation 1:
+
+#  The minimum path sum from top to bottom is 11 (i.e., 2 + 3 + 5 + 1 = 11).
+# Explanation 2:
+
+#  Only 2 can be collected.
+
+class Solution:
+    # @param A : list of list of integers
+    # @return an integer
+    def minimumTotal(self, A):
+        n = len(A)
+        dp = [[float('inf')] * n for _ in range(n)]
+
+        return self.dfs(A, n, dp, 0, 0)
+
+    
+    def dfs(self, A, n, dp, i, j):
+
+        #Base case: we have reached the last row, so return the value of the current cell
+        if i == n-1:
+            return A[i][j]
+
+        if dp[i][j] != float('inf'):
+            return dp[i][j]
+
+        left = self.dfs(A, n, dp, i+1 , j)
+        right = self.dfs(A, n, dp, i+1, j+1)  
+
+        dp[i][j] = A[i][j] = min(left, right)
+
+        return dp[i][j]
 
 
