ee2333329242233ab23210a7e6a1ab79d6fec4d2
rakesh050791 <rakesh050791@gmail.com>
2023-05-02 01:09:04 +0200

    Tree | Nodes Count of a BSF

--- Intermediate/Tree-Basics/29-August-2022.py
+++ Intermediate/Tree-Basics/29-August-2022.py
@@ -55,3 +55,85 @@ class Solution:
 
         if root.right:
             self.inOrder(root.right, result)
+
+# 3 : Nodes Count
+# You are given the root node of a binary tree A. You have to find the number of nodes in this tree.
+
+
+
+# Problem Constraints
+# 1 <= Number of nodes in the tree <= 105
+
+# 0 <= Value of each node <= 107
+
+
+
+# Input Format
+# The first and only argument is a tree node A.
+
+
+
+# Output Format
+# Return an integer denoting the number of nodes of the tree.
+
+
+
+# Example Input
+# Input 1:
+
+#  Values =  1 
+#           / \     
+#          4   3                        
+# Input 2:
+
+ 
+#  Values =  1      
+#           / \     
+#          4   3                       
+#         /         
+#        2                                     
+
+
+# Example Output
+# Output 1:
+
+#  3 
+# Output 2:
+
+#  4 
+
+
+# Example Explanation
+# Explanation 1:
+
+# Clearly, there are 3 nodes 1, 4 and 3.
+# Explanation 2:
+
+# Clearly, there are 4 nodes 1, 4, 3 and 2.
+
+
+import sys
+sys.setrecursionlimit(10**6)
+
+# Definition for a  binary tree node
+# class TreeNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.left = None
+#        self.right = None
+
+class Solution:
+    # @param A : root node of tree
+    # @return an integer
+    def solve(self, A):
+        return self.countNodes(A)
+
+    def countNodes(self, root):
+        if root == None:
+            return 0
+
+        left = self.countNodes(root.left)
+        right = self.countNodes(root.right)
+
+        return 1 + left + right
+
