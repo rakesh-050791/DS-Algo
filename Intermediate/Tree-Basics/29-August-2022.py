2966d4ec6c5add4a0930b21512020c090dcb9e0b
rakesh050791 <rakesh050791@gmail.com>
2023-05-05 21:53:22 +0200

    Tree | Counting the Nodes

--- Intermediate/Tree-Basics/29-August-2022.py
+++ Intermediate/Tree-Basics/29-August-2022.py
@@ -137,3 +137,79 @@ class Solution:
 
         return 1 + left + right
 
+
+# 4 : Counting the Nodes
+
+# Given the root of a tree A with each node having a certain value, find the count of nodes with more value than all its ancestor.
+
+
+
+# Problem Constraints
+# 1 <= Number of Nodes <= 200000
+
+# 1 <= Value of Nodes <= 2000000000
+
+
+
+# Input Format
+# The first and only argument of input is a tree node.
+
+
+
+# Output Format
+# Return a single integer denoting the count of nodes that have more value than all of its ancestors.
+
+
+
+# Example Input
+# Input 1:
+
+ 
+#      3
+# Input 2:
+
+ 
+#     4
+#    / \
+#   5   2
+#      / \
+#     3   6
+
+
+# Example Output
+# Output 1: 1 
+# Output 2: 3
+
+# Example Explanation
+# Explanation 1:
+
+#  There's only one node in the tree that is the valid node.
+# Explanation 2:
+
+#  The valid nodes are 4, 5 and 6.
+
+import sys
+sys.setrecursionlimit(10**6)
+# Definition for a  binary tree node
+# class TreeNode:
+#    def __init__(self, x):
+#        self.val = x
+#        self.left = None
+#        self.right = None
+class Solution:
+    # @param A : root node of tree
+    # @return an integer
+    def solve(self, A):
+        return self.countNodes(A, float('-inf'))
+    
+    def countNodes(self, node, maxx):
+        count = 0
+        if node == None:
+            return 0
+        else:
+            if node.val > maxx:
+                count += 1
+            count += self.countNodes(node.left, max(maxx,node.val))
+            count += self.countNodes(node.right, max(maxx,node.val))
+        
+        return count
