2833da54d82e5cd6011f3adbde32064cc6fc6ba4
rakesh050791 <rakesh050791@gmail.com>
2023-05-13 22:13:21 +0200

    Solution apporach added

--- Intermediate/July-2022/15-July-2022.py
+++ Intermediate/July-2022/15-July-2022.py
@@ -28,6 +28,23 @@ class Solution:
 # More formally, a subarray is defined as a contiguous part of an array which we can obtain by deleting zero or more elements from either end of the array.
 # A subarray sum denotes the sum of all the elements of that subarray.
 
+# Solution Approach 
+
+# For each element, if we know how many subarrays do they come in, 
+# we can easily calculate their contribution to the sum as (Number of Subarrays) * (A[i]).
+
+# How do we calculate the number of subarrays for each element?
+# Let us focus on the definition of a subarray. It is obtained by deleting zero or more elements from either end.
+# So, for each element, let X be the number of elements to their left, and Y be the number of elements to their right.
+# Number of required subarrays = (X + 1) * (Y + 1)
+
+# We can easily know X and Y from the index of the element.
+# Let the array be 0 - indexed and N be the length of the array, 
+# X = i, Y = N - i - 1
+
+# Refer to the complete solution for implementation details.
+
+
 class Solution:
     # @param A : list of integers
      # @return an long
