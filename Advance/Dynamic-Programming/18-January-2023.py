4a5976cee0555df4c6fd0ae5331a305e5cfddcd5
rakesh050791 <rakesh050791@gmail.com>
2023-05-17 23:18:54 +0200

    Dynamic programming | Max Product Subarray

--- Advance/Dynamic-Programming/18-January-2023.py
+++ Advance/Dynamic-Programming/18-January-2023.py
@@ -469,3 +469,76 @@ class Solution:
             if int(A[i-2]) == 1 or (int(A[i-2]) == 2 and int(A[i-1])>= 0 and int(A[i-1]) <= 6):
                 dp[i] = (dp[i] + dp[i-2]) % mod
         return dp[N]
+
+
+# 6 : Max Product Subarray
+
+# Given an integer array A of size N. Find the contiguous subarray within the given array (containing at least one number) which has the largest product.
+
+# Return an integer corresponding to the maximum product possible.
+
+# NOTE: Answer will fit in 32-bit integer value.
+
+
+
+# Problem Constraints
+# 1 <= N <= 5 * 105
+
+# -100 <= A[i] <= 100
+
+
+
+# Input Format
+# First and only argument is an integer array A.
+
+
+
+# Output Format
+# Return an integer corresponding to the maximum product possible.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [4, 2, -5, 1]
+# Input 2:
+
+#  A = [-3, 0, -5, 0]
+
+
+# Example Output
+# Output 1:
+
+#  8
+# Output 2:
+
+#  0
+
+
+# Example Explanation
+# Explanation 1:
+
+#  We can choose the subarray [4, 2] such that the maximum product is 8.
+# Explanation 2:
+
+#  0 will be the maximum product possible.
+
+# Solution reference : https://www.youtube.com/watch?v=lXVy6YWFcRM 
+
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def maxProduct(self, A):
+        result = max(A)
+        currMin , currMax = 1, 1
+
+        for n in A:
+            temp = n * currMax
+
+            currMax = max(n * currMax, n * currMin, n )
+            currMin = min(temp, n * currMin, n)
+
+            result = max(result, currMax)
+
+        return result
