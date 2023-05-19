d1c9d72b7718e43183510544a751fae196df4c75
rakesh050791 <rakesh050791@gmail.com>
2023-05-19 23:08:52 +0200

    Dynamic programming | Min Sum Path in Matrix

--- Advance/Dynamic-Programming/18-January-2023.py
+++ Advance/Dynamic-Programming/18-January-2023.py
@@ -664,3 +664,97 @@ class Solution:
        
    
         return dp[N][2]
+
+
+# 8 : Min Sum Path in Matrix
+# Given a M x N grid A of integers, find a path from top left to bottom right which minimizes the sum of all numbers along its path.
+
+# Return the minimum sum of the path.
+
+# NOTE: You can only move either down or right at any point in time.
+
+
+
+# Problem Constraints
+# 1 <= M, N <= 2000
+
+# -1000 <= A[i][j] <= 1000
+
+
+
+# Input Format
+# First and only argument is a 2-D grid A.
+
+
+
+# Output Format
+# Return an integer denoting the minimum sum of the path.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [
+#        [1, 3, 2]
+#        [4, 3, 1]
+#        [5, 6, 1]
+#      ]
+# Input 2:
+
+#  A = [
+#        [1, -3, 2]
+#        [2, 5, 10]
+#        [5, -5, 1]
+#      ]
+
+
+# Example Output
+# Output 1:
+
+#  8
+# Output 2:
+
+#  -1
+
+
+# Example Explanation
+# Explanation 1:
+
+#  The path will be: 1 -> 3 -> 2 -> 1 -> 1.
+# Input 2:
+
+#  The path will be: 1 -> -3 -> 5 -> -5 -> 1.
+
+# Top Down Memoization
+
+import sys
+
+sys.setrecursionlimit(10 ** 6)
+
+
+class Solution:
+    # @param A : list of list of integers
+    # @return an integer
+    def helper(self, i, j, dp, A):
+        if i < 0 or j < 0: return float('inf')
+
+        if i == 0 and j == 0:
+            dp[i][j] = A[i][j]
+            return dp[i][j]
+
+        if dp[i][j] != 0: return dp[i][j]
+
+        dp[i][j] = min(self.helper(i, j - 1, dp, A), self.helper(i - 1, j, dp, A)) + A[i][j]
+        return dp[i][j]
+
+    def minPathSum(self, A):
+        n = len(A)
+        m = len(A[0])
+
+        dp = [[0] * m for _ in range(n)]
+
+        self.helper(n - 1, m - 1, dp, A)
+        return dp[n - 1][m - 1]
+
+        # TC: O(MN); SC: O(MN)
