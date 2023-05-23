d7ef49371fac36cdd869724d0e916ea60273fb49
rakesh050791 <rakesh050791@gmail.com>
2023-05-24 00:57:33 +0200

    Dynamic-programming | Ways to send the signal

--- Advance/Dynamic-Programming/23-January-2023.py
+++ Advance/Dynamic-Programming/23-January-2023.py
@@ -84,3 +84,85 @@ class Solution:
 
         return CoinChageII(N-1,N)
 
+
+# 2 : Ways to send the signal
+# You are trying to send signals to aliens using a linear array of A laser lights. You don't know much about how the aliens are going to percieve the signals, but what you know is that if two consecutive lights are on then the aliens might take it as a sign of danger and destroy the earth.
+
+# Find and return the total number of ways in which you can send a signal without compromising the safty of the earth. Return the ans % 109 + 7.
+
+
+
+# Problem Constraints
+
+# 1 <= A <= 105
+
+
+
+# Input Format
+
+# The only argument given is integer A.
+
+
+
+# Output Format
+
+# Return the ans%(109+7).
+
+
+
+# Example Input
+
+# Input 1:
+
+#  A = 2
+# Input 2:
+
+#  A = 3
+
+
+# Example Output
+
+# Output 1:
+
+#  3
+# Output 2:
+
+#  5
+
+
+# Example Explanation
+
+# Explanation 1:
+
+#  OFF OFF
+#  OFF ON 
+#  ON OFF
+# All lights off is also a valid signal which probably means 'bye'
+
+# Explanation 2:
+
+#  OFF OFF OFF
+#  OFF OFF ON
+#  OFF ON OFF 
+#  ON OFF OFF
+#  ON OFF ON
+
+import sys
+sys.setrecursionlimit(10**6)
+class Solution:
+# @param A : integer
+# @return an integer
+    def solve(self, A):
+        MOD = 10**9 + 7
+        if A == 0:
+            return 1
+        elif A == 1:
+            return 2
+
+        dp = [0] * (A + 1)
+        dp[0] = 1
+        dp[1] = 2
+
+        for i in range(2, A + 1):
+            dp[i] = (dp[i - 1] + dp[i - 2]) % MOD
+        return dp[A]
