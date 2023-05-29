daa3e7bdcc6d7ccdc6461a00716aa9bd0c8488d6
rakesh050791 <rakesh050791@gmail.com>
2023-05-29 20:31:51 +0530

    Dynamic Programming | Unique Binary Search Trees II

--- Advance/Dynamic-Programming/30-January-2023.py
+++ Advance/Dynamic-Programming/30-January-2023.py
@@ -265,3 +265,67 @@ class Solution:
             sell2 = max(sell2, buy2+A[i])
         # Return sell2 as it represents the maximum profit we can earn after two transactions.
         return sell2
+
+
+#  4 : Unique Binary Search Trees II
+# Given an integer A, how many structurally unique BST's (binary search trees) exist that can store values 1...A?
+
+
+
+# Problem Constraints
+# 1 <= A <=18
+
+
+
+# Input Format
+# First and only argument is the integer A
+
+
+
+# Output Format
+# Return a single integer, the answer to the problem
+
+
+
+# Example Input
+# Input 1:
+
+#  1
+# Input 2:
+
+#  2
+
+
+# Example Output
+# Output 1:
+
+#  1
+# Output 2:
+
+#  2
+
+
+# Example Explanation
+# Explanation 1:
+
+#  Only single node tree is possible.
+# Explanation 2:
+
+#  2 trees are possible, one rooted at 1 and other rooted at 2.
+
+class Solution:
+    # @param A : integer
+    # @return an integer
+    def numTrees(self, A):
+        dp = [0] * (A + 1)
+        dp[0] = 1
+        dp[1] = 1
+
+        for i in range(2, A + 1):
+            val = 0
+            for j in range(i):
+                val += dp[j] * dp[i - j - 1]
+
+            dp[i] = val
+
+        return dp[A]
