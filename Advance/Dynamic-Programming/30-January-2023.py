3d54214fbffc73126d163c73bfb567283001ab07
rakesh050791 <rakesh050791@gmail.com>
2023-06-03 23:45:07 +0530

    Dynamic Programming | Matrix Chain Multiplication

--- Advance/Dynamic-Programming/30-January-2023.py
+++ Advance/Dynamic-Programming/30-January-2023.py
@@ -329,3 +329,94 @@ class Solution:
             dp[i] = val
 
         return dp[A]
+
+
+# 4 : Matrix Chain Multiplication
+
+# Given an array of integers A representing chain of 2-D matices such that the dimensions of ith matrix is A[i-1] x A[i].
+
+# Find the most efficient way to multiply these matrices together. The problem is not actually to perform the multiplications, but merely to decide in which order to perform the multiplications.
+
+# Return the minimum number of multiplications needed to multiply the chain.
+
+
+
+# Problem Constraints
+# 1 <= length of the array <= 1000
+# 1 <= A[i] <= 100
+
+
+
+# Input Format
+# The only argument given is the integer array A.
+
+
+
+# Output Format
+# Return an integer denoting the minimum number of multiplications needed to multiply the chain.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [40, 20, 30, 10, 30]
+# Input 2:
+
+#  A = [10, 20, 30]
+
+
+# Example Output
+# Output 1:
+
+#  26000
+# Output 2:
+
+#  6000
+
+
+# Example Explanation
+# Explanation 1:
+
+#  Dimensions of A1 = 40 x 20
+#  Dimensions of A2 = 20 x 30
+#  Dimensions of A3 = 30 x 10
+#  Dimensions of A4 = 10 x 30
+#  First, multiply A2 and A3 ,cost = 20*30*10 = 6000
+#  Second, multilpy A1 and (Matrix obtained after multilying A2 and A3) =  40 * 20 * 10 = 8000
+#  Third, multiply (Matrix obtained after multiplying A1, A2 and A3) and A4 =  40 * 10 * 30 = 12000
+#  Total Cost = 12000 + 8000 + 6000 =26000
+# Explanation 2:
+
+#  Cost to multiply two matrices with dimensions 10 x 20 and 20 x 30 = 10 * 20 * 30 = 6000.
+
+
+class Solution:
+    # @param A : list of integers
+    # @return an integer
+    def solve(self, A):
+        N=len(A)
+        dp=[[None]*N for i in range(N)]  # Initialize table for minimum costs
+        
+        def MCM(A,i,j):
+            if i==j:  # Base case: subchain of length 1 costs 0 to multiply
+                return 0
+            
+            if dp[i][j]:  # Check if minimum cost has already been computed
+                return dp[i][j]
+            
+            min_cost = float('inf')
+            for k in range(i,j):
+                # Compute cost of multiplying subchains A[i:k+1] and A[k+1:j+1]
+                cost = MCM(A,i,k) + MCM(A,k+1,j) + A[i-1]*A[k]*A[j]
+                
+                if cost < min_cost:  # Update minimum cost and optimal split
+                    min_cost = cost
+                    dp[i][j] = min_cost
+            
+            return min_cost  
+        
+        ans = MCM(A,1,N-1)  # Compute minimum cost of multiplying entire chain
+        
+        return ans  
+
