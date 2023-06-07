a3bb731827d252b12fcbee1b85800781935dd40e
rakesh050791 <rakesh050791@gmail.com>
2023-06-07 22:36:34 +0530

    Dynamic programming | Coin Sum Infinite

--- Advance/Dynamic-Programming/23-January-2023.py
+++ Advance/Dynamic-Programming/23-January-2023.py
@@ -417,3 +417,76 @@ class Solution:
 
         if recurBro(0,0): return 1
         return 0
+
+# 6 : Coin Sum Infinite
+# You are given a set of coins A. In how many ways can you make sum B assuming you have infinite amount of each coin in the set.
+
+# NOTE:
+
+# Coins in set A will be unique. Expected space complexity of this problem is O(B).
+# The answer can overflow. So, return the answer % (106 + 7).
+
+
+# Problem Constraints
+# 1 <= A <= 500
+# 1 <= A[i] <= 1000
+# 1 <= B <= 50000
+
+
+
+# Input Format
+# First argument is an integer array A representing the set.
+# Second argument is an integer B.
+
+
+
+# Output Format
+# Return an integer denoting the number of ways.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [1, 2, 3]
+#  B = 4
+# Input 2:
+
+#  A = [10]
+#  B = 10
+
+
+# Example Output
+# Output 1:
+
+#  4
+# Output 2:
+
+#  1
+
+
+# Example Explanation
+# Explanation 1:
+
+#  The 4 possible ways are:
+#  {1, 1, 1, 1}
+#  {1, 1, 2}
+#  {2, 2}
+#  {1, 3} 
+# Explanation 2:
+
+#  There is only 1 way to make sum 10.
+
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return an integer
+    def coinchange2(self, coins, amount):
+        N = len(coins)
+        dp = [0 for _ in range(amount+1)]
+        dp[0] = 1
+        for indx  in range(N):
+            for coins_used in range(1,amount+1):
+                if coins_used >= coins[indx]:
+                    dp[coins_used] += dp[coins_used - coins[indx]]
+        return dp[amount] % 1000007
