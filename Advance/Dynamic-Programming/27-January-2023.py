864e92157b3c96178d199319e2e24bf00cbe29ac
rakesh050791 <rakesh050791@gmail.com>
2023-06-12 23:23:28 +0530

    Longest Increasing Subsequence

--- Advance/Dynamic-Programming/27-January-2023.py
+++ Advance/Dynamic-Programming/27-January-2023.py
@@ -100,3 +100,73 @@ class Solution:
         return LIS(0,-1)
 
 
+# 2 : Longest Increasing Subsequence
+
+# Find the longest increasing subsequence of a given array of integers, A.
+
+# In other words, find a subsequence of array in which the subsequence's elements are in strictly increasing order, and in which the subsequence is as long as possible.
+
+# In this case, return the length of the longest increasing subsequence.
+
+
+
+# Problem Constraints
+# 1 <= length(A) <= 2500
+# 0 <= A[i] <= 2500
+
+
+
+# Input Format
+# The first and the only argument is an integer array A.
+
+
+
+# Output Format
+# Return an integer representing the length of the longest increasing subsequence.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [1, 2, 1, 5]
+# Input 2:
+
+#  A = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+
+
+# Example Output
+# Output 1:
+
+#  3
+# Output 2:
+
+#  6
+
+
+# Example Explanation
+# Explanation 1:
+
+#  The longest increasing subsequence: [1, 2, 5]
+# Explanation 2:
+
+#  The possible longest increasing subsequences: [0, 2, 6, 9, 13, 15] or [0, 4, 6, 9, 11, 15] or [0, 4, 6, 9, 13, 15]
+
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def lis(self, A):
+        # Initialize a list rs with length equal to the length of the input array A
+        rs = [1] * (len(A))
+       
+        # Iterate over the input array A
+        for i in range(1, len(A)):
+            # For each element A[i], compare it with all previous elements A[j] where j ranges from 0 to i-1
+            for j in range(i):
+                # If A[j] is less than A[i], update the value of rs[i]
+                if A[j] < A[i]:
+                    # Update the value of rs[i] as the maximum of its current value and (rs[j] + 1)
+                    rs[i] = max(rs[j] + 1, rs[i])
+       
+        # Return the maximum value in the list rs
+        return max(rs)
