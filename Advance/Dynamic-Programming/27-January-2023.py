b24a894f0fb9f0c8d907126279d67e58ee55472a
rakesh050791 <rakesh050791@gmail.com>
2023-06-13 23:14:53 +0530

    DYNAMIC PROGRAMMING | Palindrome Partitioning II

--- Advance/Dynamic-Programming/27-January-2023.py
+++ Advance/Dynamic-Programming/27-January-2023.py
@@ -170,3 +170,85 @@ class Solution:
        
         # Return the maximum value in the list rs
         return max(rs)
+
+
+
+# 3 : Palindrome Partitioning II
+
+# Given a string A, partition A such that every substring of the partition is a palindrome.
+
+# Return the minimum cuts needed for a palindrome partitioning of A.
+
+
+
+# Problem Constraints
+# 1 <= length(A) <= 501
+
+
+
+# Input Format
+# The first and the only argument contains the string A.
+
+
+
+# Output Format
+# Return an integer, representing the minimum cuts needed.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = "aba"
+# Input 2:
+
+#  A = "aab"
+
+
+# Example Output
+# Output 1:
+
+#  0
+# Output 2:
+
+#  1
+
+
+# Example Explanation
+# Explanation 1:
+
+#  "aba" is already a palindrome, so no cuts are needed.
+# Explanation 2:
+
+#  Return 1 since the palindrome partitioning ["aa","b"] could be produced using 1 cut.
+
+class Solution:
+    def isPalindrome(self, A):
+        dp = []
+        n = len(A)
+        for i in range(n):
+            dp.append([True] * n)
+        for l in range(2, n+1): # considering window size from 2 to n(inclusive)
+            for start in range(n - l + 1): # last index of start will be n-l (inclusive)
+                end = start + l - 1
+                dp[start][end] = A[start] == A[end] and dp[start + 1][end - 1]
+        return dp
+
+    # @param A : string
+    # @return an integer
+    def minCut(self, A):
+        n = len(A)
+        isPalindrome = self.isPalindrome(A)
+        dp = [-1 for i in range(n)]
+        dp[0] = 0
+        for i in range(n):
+            if isPalindrome[0][i]:
+                dp[i] = 0
+            else:
+                mini = float('inf')
+                for j in range(i, 0, -1):
+                    if isPalindrome[j][i]: # get the min cut for all the substrings before i
+                        if dp[j - 1] < mini:
+                            mini = dp[j - 1]
+                dp[i] = mini + 1
+        return dp[n - 1]
