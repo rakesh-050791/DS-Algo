cc40e4aee90fea3208694f21a9c1d5494e3e65f2
rakesh050791 <rakesh050791@gmail.com>
2023-06-17 21:22:56 +0530

    Dynamic Programming | Flip Array

--- Advance/Dynamic-Programming/18-January-2023.py
+++ Advance/Dynamic-Programming/18-January-2023.py
@@ -762,6 +762,7 @@ class Solution:
 
         # TC: O(MN); SC: O(MN)
 
+
 # 9 : Unbounded Knapsack
 
 # Given a knapsack weight A and a set of items with certain value B[i] and weight C[i], we need to calculate maximum amount that could fit in this quantity.
@@ -845,3 +846,88 @@ class Solution:
                 
         # Return the maximum value that can be obtained by filling the knapsack of weight `A` with items from the given set
         return ds[A]
+
+
+# 9 : Flip Array
+
+# Given an array A of positive elements, you have to flip the sign of some of its elements such that the resultant sum of the elements of array should be minimum non-negative(as close to zero as possible).
+
+# Return the minimum number of elements whose sign needs to be flipped such that the resultant sum is minimum non-negative.
+
+
+
+# Problem Constraints
+
+# 1 <= length of(A) <= 100
+
+# Sum of all the elements will not exceed 10,000.
+
+
+
+# Input Format
+
+# First and only argument is an integer array A.
+
+
+
+# Output Format
+
+# Return an integer denoting the minimum number of elements whose sign needs to be flipped.
+
+
+
+# Example Input
+
+# Input 1:
+
+#  A = [15, 10, 6]
+# Input 2:
+
+#  A = [14, 10, 4]
+
+
+# Example Output
+
+# Output 1:
+
+#  1
+# Output 2:
+
+#  1
+
+
+# Example Explanation
+
+# Explanation 1:
+
+#  Here, we will flip the sign of 15 and the resultant sum will be 1.
+# Explanation 2:
+
+#  Here, we will flip the sign of 14 and the resultant sum will be 0.
+#  Note that flipping the sign of 10 and 4 also gives the resultant sum 0 but flippings there sign are not minimum.
+
+
+class Solution:
+    # @param A : tuple of integers
+    # @return an integer
+    def solve(self, A):
+        n = len(A)
+        arrsum = sum(A)
+        target = arrsum // 2
+
+        dp = [[n + 1 for _ in range(target + 1)] for _ in range(n + 1)]
+        for i in range(n + 1):
+            dp[i][0] = 0
+
+        for i in range(1, n + 1):
+            for j in range(1, target + 1):
+                if A[i-1] <= j:
+                    dp[i][j] = min(1 + dp[i-1][j-A[i-1]], dp[i-1][j])
+                else:
+                    dp[i][j] = dp[i-1][j]
+
+        for i in range(target, -1, -1):
+            if dp[n][i] < n:
+                return dp[n][i]
+
+
