55d1529f381dc151fa3371c23aaf86de8efff0c2
rakesh050791 <rakesh050791@gmail.com>
2023-06-18 17:07:48 +0530

    Dynamic Programming | Buying Candies

--- Advance/Dynamic-Programming/18-January-2023.py
+++ Advance/Dynamic-Programming/18-January-2023.py
@@ -931,3 +931,87 @@ class Solution:
                 return dp[n][i]
 
 
+# 10 : Buying Candies
+
+# Rishik likes candies a lot. So, he went to a candy-shop to buy candies.
+
+# The shopkeeper showed him N packets each containg A[i] candies for cost of C[i] nibbles, each candy in that packet has a sweetness B[i]. The shopkeeper puts the condition that Rishik can buy as many complete candy-packets as he wants but he can't buy a part of the packet.
+
+# Rishik has D nibbles, can you tell him the maximum amount of sweetness he can get from candy-packets he will buy?
+
+
+# Problem Constraints
+# 1 <= N <= 700
+
+# 1 <= A[i] <= 1000
+
+# 1 <= B[i] <= 1000
+
+# 1 <= C[i],D <= 1000
+
+
+
+# Input Format
+# First argument of input is an integer array A
+# Second argument of input is an integer array B
+# Third argument of input is an integer array C
+# Fourth argument of input is an integer D
+
+
+# Output Format
+# Return a single integer denoting maximum sweetness of the candies that he can buy
+
+
+# Example Input
+# Input 1:
+
+#  A = [1, 2, 3]
+#  B = [2, 2, 10]
+#  C = [2, 3, 9]
+#  D = 8
+# Input 2:
+
+#  A = [2]
+#  B = [5]
+#  C = [10]
+#  D = 99
+
+
+# Example Output
+# Output 1:
+
+#  10
+# Output 2:
+
+#  90
+
+
+# Example Explanation
+# Explanation 1:
+
+#  Choose 1 Packet of kind 1 = 1 Candy of 2 Sweetness = 2 Sweetness
+#  Choose 2 Packet of kind 2 = 2 Candy of 2 Sweetness = 8 Sweetness
+# Explanation 2:
+
+#  Buy 9 Packet of kind 1. 18 Candy each of 5 Sweetness = 90 Sweetness
+
+
+class Solution:
+    # @param A : list of integers (number of candies in each packet)
+    # @param B : list of integers (sweetness of each candy in each packet)
+    # @param C : list of integers (cost of each packet in nibbles)
+    # @param D : integer (total number of nibbles Rishik has)
+    # @return an integer (maximum sweetness Rishik can get)
+
+    def solve(self, A, B, C, D):
+        N = len(A)  # number of candy packets
+        sweetness=[A[i]*B[i] for i in range(N)]  # list to store sweetness of each candy packet
+        dp = [0] * (D + 1)  # list to store the maximum sweetness for each number of nibbles from 0 to D
+
+        for i in range(N):  # iterate over each candy packet in the shop
+            for j in range(C[i], D + 1):  # iterate over each possible number of nibbles from C[i] to D
+                # if we don't buy the current candy packet, we keep the same sweetness as before
+                # otherwise, we add the sweetness of the candy packet to the best possible solution we can get with the remaining nibbles
+                dp[j] = max(dp[j], dp[j - C[i]] + sweetness[i])
+
+        return dp[D]  # return the maximum sweetness we can get with D nibbles
