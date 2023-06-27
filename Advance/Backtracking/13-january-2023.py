b8fcdbc52cd03eea87db7dc0c1855d920ebafeee
rakesh050791 <rakesh050791@gmail.com>
2023-06-27 21:26:25 +0200

    Backtracking | Combination Sum II

--- Advance/Backtracking/13-january-2023.py
+++ Advance/Backtracking/13-january-2023.py
@@ -408,3 +408,120 @@ class Solution:
         return res
 
         # TC: O(2^B); SC: O(B)
+
+
+# 5 : Combination Sum II
+
+# Given an array of size N of candidate numbers A and a target number B. Return all unique combinations in A where the candidate numbers sums to B.
+
+# Each number in A may only be used once in the combination.
+
+# Note:
+
+# All numbers (including target) will be positive integers.
+# Elements in a combination (a1, a2, … , ak) must be in non-descending order. (ie, a1 ≤ a2 ≤ … ≤ ak).
+# The solution set must not contain duplicate combinations.
+# Warning:
+
+# DO NOT USE LIBRARY FUNCTION FOR GENERATING COMBINATIONS.
+
+# Example : itertools.combinations in python. If you do, we will disqualify your submission and give you penalty points.
+
+
+
+# Problem Constraints
+# 1 <= N <= 20
+
+
+
+# Input Format
+# First argument is an integer array A denoting the collection of candidate numbers.
+# Second argument is an integer which represents the target number.
+
+
+
+# Output Format
+# Return all unique combinations in A where the candidate numbers sums to B.
+
+
+
+# Example Input
+# Input 1:
+
+#  A = [10, 1, 2, 7, 6, 1, 5]
+#  B = 8
+# Input 2:
+
+#  A = [2, 1, 3]
+#  B = 3
+
+
+# Example Output
+# Output 1:
+
+#  [ 
+#   [1, 1, 6 ],
+#   [1, 2, 5 ],
+#   [1, 7 ], 
+#   [2, 6 ] 
+#  ]
+# Output 2:
+
+#  [
+#   [1, 2 ],
+#   [3 ]
+#  ]
+
+
+# Example Explanation
+# Explanation 1:
+
+#  1 + 1 + 6 = 8
+#  1 + 2 + 5 = 8
+#  1 + 7 = 8
+#  2 + 6 = 8
+#  All the above combinations sum to 8 and are arranged in ascending order.
+# Explanation 2:
+
+#  1 + 2 = 3
+#  3 = 3
+#  All the above combinations sum to 3 and are arranged in ascending order.
+
+
+class Solution:
+    # @param A : list of integers
+    # @param B : integer
+    # @return a list of list of integers
+    def combinationSum(self, A, B):
+        res = []
+        N   = len(A)
+        A.sort()
+        def subbsetSum(indx,arr,curSum):
+            if indx == N:
+                if curSum == B:
+                    temp = arr.copy()
+                    res.append(temp)
+                return
+            
+            if curSum > B:
+                return
+            
+            # take it 
+            arr.append(A[indx])
+            curSum += A[indx]
+            
+            subbsetSum(indx+1,arr,curSum)
+            
+            # skipping duplicates for right side call 
+            while indx < N-1 and A[indx] == A[indx+1]:
+                indx += 1
+            
+            # dont take it and backtrack 
+            curSum -= A[indx]
+            arr.pop()
+            subbsetSum(indx+1,arr,curSum)
+            
+        
+        subbsetSum(0,[],0)
+        return res
+
